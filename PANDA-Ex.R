153a4335e6f57fb4961b0413c83dc16bd981af71
Helene Morlon <morlon.helene@gmail.com>
2014-02-11 19:49:15 +0100

    not sure what that is

--- PANDA-Ex.R
+++ PANDA-Ex.R
@@ -5,8 +5,8 @@ library('PANDA')
 
 base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
 cleanEx()
-nameEx("Cetaceae")
-### * Cetaceae
+nameEx("Cetacea")
+### * Cetacea
 
 flush(stderr()); flush(stdout())
 
@@ -49,20 +49,19 @@ nameEx("fit_bd")
 flush(stderr()); flush(stdout())
 
 ### Name: fit_bd
-### Title: Fit parameters for cladogenesis birth-death proces
+### Title: Maximum likelihood fit of the general birth-death model
 ### Aliases: fit_bd
 
 ### ** Examples
 
-# Currently this test is too much CPU consuming
-# Need to perform it
-data(Cetaceae)
-#first model
+data(Cetacea)
+#fit of a pure birth model (no extinction) with an exponential variation of speciation rate with time
+tot_time<-max(node.age(Cetacea)$ages)
 f.lamb <-function(x,y){y[1] * exp(y[2] * x)}
 f.mu<-function(x,y){0}
 lamb_par<-c(0.05, 0.01)
 mu_par<-c()
-result <- fit_bd(Cetaceae,max(node.age(Cetaceae)$ages),f.lamb,f.mu,
+fit_bd(Cetacea,tot_time,f.lamb,f.mu,
        lamb_par,mu_par,f=87/89, expo.lamb = TRUE, fix.mu=TRUE)
 
 
@@ -79,8 +78,8 @@ flush(stderr()); flush(stdout())
 
 ### ** Examples
 
-data(Cetaceae)
-result <- fit_coal_cst_mod(Cetaceae, tau0=1.5, N0=1e6)
+data(Cetacea)
+result <- fit_coal_cst_mod(Cetacea, tau0=1.5, N0=1e6)
 print(result)
 
 
@@ -97,8 +96,8 @@ flush(stderr()); flush(stdout())
 
 ### ** Examples
 
-data(Cetaceae)
-result <- fit_coal_exp_mod(Cetaceae, tau0=1.e-3, gamma=-1)
+data(Cetacea)
+result <- fit_coal_exp_mod(Cetacea, tau0=1.e-3, gamma=-1)
 print(result)
 
 
@@ -115,8 +114,8 @@ flush(stderr()); flush(stdout())
 
 ### ** Examples
 
-data(Cetaceae)
-result <- fit_coal_var(Cetaceae, lamb0=0.01, alpha=-0.001, mu0=0.0, beta=0, N0=1e6)
+data(Cetacea)
+result <- fit_coal_var(Cetacea, lamb0=0.01, alpha=-0.001, mu0=0.0, beta=0, N0=1e6)
 print(result)
 
 
@@ -134,15 +133,15 @@ flush(stderr()); flush(stdout())
 
 ### ** Examples
 
-data(Cetaceae)
-tot_time <- max(node.age(Cetaceae)$ages)
+data(Cetacea)
+tot_time <- max(node.age(Cetacea)$ages)
 lamb_par <- c(1, -3)
 # lambda(t) and mu(t)
 f.lamb <- function(t){lamb_par[1] * exp(lamb_par[2] * t)}
 f.mu <- function(t){0}
 f <- 87/88
 # model with none extinction & exponential speciation variation
-likelihood <- likelihood_bd(Cetaceae,tot_time,f.lamb,f.mu,f,cst.mu=TRUE,expo.lamb=TRUE)
+likelihood <- likelihood_bd(Cetacea,tot_time,f.lamb,f.mu,f,cst.mu=TRUE,expo.lamb=TRUE)
 print(likelihood)
 # [1] -2160.751
 
