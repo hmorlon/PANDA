2831ea7e840198f2ea373b1f49c22b1e3ad77125
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2014-03-12 11:35:38 +0100

    Cancelled effects of previous commit due to several numerical troubles

--- R/fit_bd.R
+++ R/fit_bd.R
@@ -17,12 +17,12 @@ fit_bd <-
     {
       lamb_par <- init[1:length(lamb_par)]
       mu_par <- init[(1+length(lamb_par)):length(init)]
-#       f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
-#       f.mu.par <- function(t){abs(f.mu(t,mu_par))}
-      f.lamb.abs.scal <- function(t){abs(f.lamb(t,lamb_par))}
-      f.lamb.par <- function(t){apply(X=as.matrix(t), MARGIN=1, FUN=f.lamb.abs.scal)}
-      f.mu.abs.scal <- function(t){abs(f.mu(t,mu_par))}
-      f.mu.par <- function(t){apply(X=as.matrix(t), MARGIN=1, FUN=f.mu.abs.scal)}
+      f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
+      f.mu.par <- function(t){abs(f.mu(t,mu_par))}
+# #       f.lamb.abs.scal <- function(t){abs(f.lamb(t,lamb_par))}
+# #       f.lamb.par <- function(t){mapply(f.lamb.abs.scal, t)}
+# #       f.mu.abs.scal <- function(t){abs(f.mu(t,mu_par))}
+# #       f.mu.par <- function(t){mapply(f.mu.abs.scal,t)}
       LH <- likelihood_bd(phylo,tot_time,f.lamb.par,f.mu.par,f,cst.lamb=cst.lamb,cst.mu=cst.mu,expo.lamb=expo.lamb,expo.mu=expo.mu,cond=cond)
       return(-LH)
     }
@@ -37,12 +37,12 @@ fit_bd <-
     optimLH <- function(init)
     {
       lamb_par <- init[1:length(lamb_par)]
-#       f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
-#       f.mu.par <- function(t){abs(f.mu(t,mu_par))}
-      f.lamb.abs.scal <- function(t){abs(f.lamb(t,lamb_par))}
-      f.lamb.par <- function(t){apply(X=as.matrix(t), MARGIN=1, FUN=f.lamb.abs.scal)}
-      f.mu.abs.scal <- function(t){abs(f.mu(t,mu_par))}
-      f.mu.par <- function(t){apply(X=as.matrix(t), MARGIN=1, FUN=f.mu.abs.scal)}
+      f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
+      f.mu.par <- function(t){abs(f.mu(t,mu_par))}
+#       f.lamb.abs.scal <- function(t){abs(f.lamb(t,lamb_par))}
+#       f.lamb.par <- function(t){mapply(f.lamb.abs.scal, t)}
+#       f.mu.abs.scal <- function(t){abs(f.mu(t,mu_par))}
+#       f.mu.par <- function(t){mapply(f.mu.abs.scal,t)}
       LH <- likelihood_bd(phylo,tot_time,f.lamb.par,f.mu.par,f,cst.lamb=cst.lamb,cst.mu=TRUE,expo.lamb=expo.lamb,cond=cond)
       return(-LH)
     }
