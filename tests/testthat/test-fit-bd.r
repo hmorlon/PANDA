adcb4e6d03bc1bd9fcee28583397984c94a7d240
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2014-08-28 16:24:08 +0200

    updated test-fit-bd: removed comments, changed control parameters. WIP

--- tests/testthat/test-fit-bd.r
+++ tests/testthat/test-fit-bd.r
@@ -1,11 +1,11 @@
 # Load data and get tot_time
 data(Cetacea)
 tot_time<-max(node.age(Cetacea)$ages)
-options(digits=16)
+options(digits=17)
 
 # Parameters for validation
-precision_lh <- 1e-3
-precision_aicc <- 5e-2
+precision_lh <- 3e-3
+precision_aicc <- 2e-1
 
 
 context("B constant")
@@ -14,11 +14,11 @@ f.lamb <-function(t,y){y[1]}
 f.mu<-function(t,y){0}
 lamb_par <- c(0.09)
 mu_par <- c()
-res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.lamb = TRUE, fix.mu=TRUE, cond="stem")
+res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.lamb=TRUE,fix.mu=TRUE,cond="stem")
 
 reference_lh <- -279.0280
 reference_aicc <- 560.1031
-# reference_aicc <- 560.0796
+
 test_that("B constant",{
   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
   expect_that( abs(res$aicc - reference_aicc)  , is_less_than(precision_aicc) )
@@ -30,10 +30,9 @@ f.lamb <-function(t,y){y[1]}
 f.mu<-function(t,y){y[1]}
 lamb_par <- c(0.09)
 mu_par <- c(0.3)
-res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.lamb=TRUE, cst.mu=TRUE, cond="stem")
+res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89,cst.lamb=TRUE,cst.mu=TRUE,cond="stem")
 reference_lh <- -279.0280
 reference_aicc <- 562.1989
-# reference_aicc <- 562.1270
 
 test_that("BD constant",{
   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
@@ -50,7 +49,6 @@ mu_par<-c()
 res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, expo.lamb=TRUE, fix.mu=TRUE, cond="stem")
 reference_lh <- -278.9887
 reference_aicc <- 562.0485
-# reference_aicc <- 562.1213
 
 test_that("B variable exponential",{
   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
@@ -58,21 +56,19 @@ test_that("B variable exponential",{
 })
 
 # 4) B variable L
-## TODO fix numerical results
-# context("B linear")
-# f.lamb <-function(t,y){y[1] + y[2] * t}
-# f.mu<-function(t,y){0}
-# lamb_par<-c(0.05, 0.01)
-# mu_par<-c()
-# res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, fix.mu=TRUE, cond="stem")
-# reference_lh <- -278.9896
-# reference_aicc <- 562.0502
-# # reference_aicc <- 562.0485
-#
-# test_that("B variable linear",{
-#   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
-#   expect_that( abs(res$aicc - reference_aicc)  , is_less_than(precision_aicc) )
-# })
+context("B linear")
+f.lamb <-function(t,y){y[1] + y[2] * t}
+f.mu<-function(t,y){0}
+lamb_par<-c(0.05, 0.01)
+mu_par<-c()
+res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, fix.mu=TRUE, cond="stem")
+reference_lh <- -278.9896
+reference_aicc <- 562.0502
+
+test_that("B variable linear",{
+  expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
+  expect_that( abs(res$aicc - reference_aicc)  , is_less_than(precision_aicc) )
+})
 
 
 # 5) B variable E, D constant
@@ -85,7 +81,6 @@ mu_par<-c(0.1)
 res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, expo.lamb=TRUE, cst.mu=TRUE, cond="stem")
 reference_lh <- -278.9887
 reference_aicc <- 564.1204
-# reference_aicc <- 564.2676
 
 test_that("B variable exponential, D constant",{
   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
@@ -94,21 +89,19 @@ test_that("B variable exponential, D constant",{
 
 
 # 6) B variable L, D constant
-## TODO fix numerical results
-# context("B linear, D constant")
-# f.lamb <-function(t,y){y[1] + y[2] * t}
-# f.mu<-function(t,y){y[1]}
-# lamb_par<-c(0.5, 0.01)
-# mu_par <- c(0.5)
-# res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.mu= TRUE, cond="stem")
-# reference_lh <- -278.9896
-# reference_aicc <- 564.1221
-# # reference_aicc <- 564.2676
-#
-# test_that("B variable linear, D constant",{
-#   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
-#   expect_that( abs(res$aicc - reference_aicc)  , is_less_than(precision_aicc) )
-# })
+context("B linear, D constant")
+f.lamb <-function(t,y){y[1] + y[2] * t}
+f.mu<-function(t,y){y[1]}
+lamb_par<-c(0.5, 0.01)
+mu_par <- c(0.5)
+res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.mu= TRUE, cond="stem")
+reference_lh <- -278.9896
+reference_aicc <- 564.1221
+
+test_that("B variable linear, D constant",{
+  expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
+  expect_that( abs(res$aicc - reference_aicc)  , is_less_than(precision_aicc) )
+})
 
 
 # 7) B constant, D variable E
@@ -120,7 +113,6 @@ mu_par <-c(0.5, 0.01)
 res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.lamb=TRUE, expo.mu=TRUE, cond="stem")
 reference_lh <- -279.0280
 reference_aicc <- 564.1989
-# reference_aicc <- 564.2676
 
 test_that("B constant, D exponential",{
   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
@@ -129,18 +121,16 @@ test_that("B constant, D exponential",{
 
 
 # 8) B constant, D variable L
-## TODO fix numerical results
-# context("B constant, D linear")
-# f.lamb<-function(t,y){y[1]}
-# f.mu <-function(t,y){y[1] + y[2] * t}
-# lamb_par <- c(0.5)
-# mu_par <-c(0.5, 0.01)
-# res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.lamb=TRUE, cond="stem")
-# reference_lh <- -279.0280
-# reference_aicc <- 564.1989
-# # reference_aicc <- 564.2676
-#
-# test_that("B constant, D linear",{
-#   expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
-#   expect_that( abs(res$aicc - reference_aicc)  , is_less_than(precision_aicc) )
-# })
+context("B constant, D linear")
+f.lamb<-function(t,y){y[1]}
+f.mu <-function(t,y){y[1] + y[2] * t}
+lamb_par <- c(0.5)
+mu_par <-c(0.5, 0.01)
+res <- fit_bd(Cetacea,tot_time,f.lamb,f.mu,lamb_par,mu_par,f=87/89, cst.lamb=TRUE, cond="stem")
+reference_lh <- -279.0280
+reference_aicc <- 564.1989
+
+test_that("B constant, D linear",{
+  expect_that( abs(res$LH - reference_lh)  , is_less_than(precision_lh) )
+  expect_that( abs(res$aicc - reference_aicc)  , is_less_than(precision_aicc) )
+})
