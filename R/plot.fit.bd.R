9bb7dd00a061e0b738a9f94e8a9ee94a1e1f12cd
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2014-09-03 15:04:18 +0200

    cosmetics. Changed argument names to prevent from check warnings. WIP

--- R/plot.fit.bd.R
+++ R/plot.fit.bd.R
@@ -1,11 +1,11 @@
-plot.fit.bd <- function(res,tot_time)
+plot.fit.bd <- function(fit.bd,tot_time)
 {
   t <- seq(0,tot_time, length.out=100)
   X11()
-  plot(t, res$f.lamb(t), type='l', xlab="Time", ylab="Speciation rate", xlim=c(tot_time,0),main="Fitted speciation rate")
+  plot(t, fit.bd$f.lamb(t), type='l', xlab="Time", ylab="Speciation rate", xlim=c(tot_time,0),main="Fitted speciation rate")
   X11()
-  plot(t, res$f.mu(t), type='l', xlab="Time", ylab="Extinction rate", xlim=c(tot_time,0),main="Fitted extinction rate")
-  r <- function(t) {res$f.lamb(t) - res$f.mu(t)}
+  plot(t, fit.bd$f.mu(t), type='l', xlab="Time", ylab="Extinction rate", xlim=c(tot_time,0),main="Fitted extinction rate")
+  r <- function(t) {fit.bd$f.lamb(t) - fit.bd$f.mu(t)}
   X11()
   plot(t, r(t), type='l', xlab="Time", ylab="Diversification rate", xlim=c(tot_time,0),main="Fitted diversification rate")
 }
