d4adf3b9b93312950fe41ceb3cd43e5f3a2c5293
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2014-10-02 14:12:18 +0200

    Replaced X11 by dev.new, which has the same role: X11 is exclusively used by unix system wheras quartz is the equivalent in MacOS, windows in Windows system

--- R/plot_dtt.R
+++ R/plot_dtt.R
@@ -4,7 +4,7 @@ plot_dtt <- function(fit.bd,tot_time, N0=1)
   r <- function(t) {-fit.bd$f.lamb(t) + fit.bd$f.mu(t)}
   R <- function(s){.Integrate(r,0,s)}
   N <- N0 * exp(Vectorize(R)(t))
-  X11()
+  dev.new()
   plot(t, N, type='l', xlab="Time", ylab="dtt", xlim=c(tot_time,0),main="Diversification Through Time")
   grid()
 }
