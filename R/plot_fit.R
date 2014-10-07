6cb675aa5b02f280806379af1430d6b0324dd84e
Helene Morlon <morlon@biologie.ens.fr>
2014-10-07 12:45:35 +0200

    various edits to plot_fit function and manual

--- R/plot_fit.R
+++ R/plot_fit.R
@@ -4,10 +4,10 @@ plot_fit <- function(fit.bd,tot_time)
       stop("object \"fit.bd\" is not of class \"fit.bd\"")
   t <- seq(0,tot_time, length.out=100)
   dev.new()
-  plot(t, fit.bd$f.lamb(t), type='l', xlab="Time", ylab="Speciation rate", xlim=c(tot_time,0),main="Fitted speciation rate")
+  plot(t, fit.bd$f.lamb(t), type='l', xlab="time", ylab="speciation rate", xlim=c(tot_time,0),main="Fitted speciation rate")
   dev.new()
-  plot(t, fit.bd$f.mu(t), type='l', xlab="Time", ylab="Extinction rate", xlim=c(tot_time,0),main="Fitted extinction rate")
+  plot(t, fit.bd$f.mu(t), type='l', xlab="time", ylab="extinction rate", xlim=c(tot_time,0),main="Fitted extinction rate")
   r <- function(t) {fit.bd$f.lamb(t) - fit.bd$f.mu(t)}
   dev.new()
-  plot(t, r(t), type='l', xlab="Time", ylab="Diversification rate", xlim=c(tot_time,0),main="Fitted diversification rate")
+  plot(t, r(t), type='l', xlab="time", ylab="net diversification rate", xlim=c(tot_time,0),main="Fitted net diversification rate")
 }
