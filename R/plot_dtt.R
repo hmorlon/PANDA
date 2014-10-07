67f985001cd6cd036e290e6e5313d77a26ef1273
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2014-10-07 14:19:18 +0200

    Various edit to plot_dtt: function &  manual

--- R/plot_dtt.R
+++ R/plot_dtt.R
@@ -1,10 +1,9 @@
-plot_dtt <- function(fit.bd,tot_time, N0=1)
+plot_dtt <- function(fit.bd,tot_time,N0=1)
 {
   t <- seq(0,tot_time, length.out=100)
   r <- function(t) {-fit.bd$f.lamb(t) + fit.bd$f.mu(t)}
   R <- function(s){.Integrate(r,0,s)}
   N <- N0 * exp(Vectorize(R)(t))
   dev.new()
-  plot(t, N, type='l', xlab="Time", ylab="dtt", xlim=c(tot_time,0),main="Diversification Through Time")
-  grid()
+  plot(t, N, type='l', xlab="time", ylab="number of species", xlim=c(tot_time,0),main="Diversity Through Time")
 }
