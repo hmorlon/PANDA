e302e62b3d8bb5feba6b33569c39121921af1575
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2014-11-04 14:08:09 +0100

    Fixed bug in fit_env_bd: function definition was modified

--- R/fit_env_bd.R
+++ R/fit_env_bd.R
@@ -30,8 +30,8 @@ fit_env_bd <- function (phylo, env_data, tot_time, f.lamb, f.mu, lamb_par, mu_pa
     index <- 1 + as.integer( (t - a) * n / (b - a))
     return(env_tabulated[index])
   }
-  f.lamb.env <- function(t,y){ f.lamb(t, env_func_tab(t), lamb_par)}
-  f.mu.env <- function(t,y){ f.mu(t, env_func_tab(t), mu_par)}
+  f.lamb.env <- function(t,y){ f.lamb(t, env_func_tab(t), y)}
+  f.mu.env <- function(t,y){ f.mu(t, env_func_tab(t), y)}
   res <- fit_bd(phylo, tot_time, f.lamb.env, f.mu.env, lamb_par, mu_par, f,
            meth, cst.lamb, cst.mu, expo.lamb, expo.mu, fix.mu, dt, cond)
   res$model <- "environmental birth death"
