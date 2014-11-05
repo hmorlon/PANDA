7d0132034e63841f3a0530943c375f1528aff2c4
Helene Morlon <morlon@biologie.ens.fr>
2014-11-05 18:47:53 +0100

    use fit.env instead of x in plot_fit_env function

--- R/plot_fit_env.R
+++ R/plot_fit_env.R
@@ -4,22 +4,22 @@ plot_fit_env <- function(fit.env, env_data, tot_time)
       stop("object is not of class \"fit.env\"")
   t <- seq(0,tot_time, length.out=100)
   dev.new()
-  plot(-t, x$f.lamb(t), type='l', xlab="time", ylab="speciation rate", main="Fitted speciation rate")
+  plot(-t, fit.env$f.lamb(t), type='l', xlab="time", ylab="speciation rate", main="Fitted speciation rate")
   # Plot f.lamb(env_data)
-  df <- smooth.spline(x=env_data[,1], env_data[,2])$df
+  df <- smooth.spline(env_data[,1], env_data[,2])$df
   spline_result <- sm.spline(env_data[,1],env_data[,2], df=df)
   env_func <- function(t){predict(spline_result,t)}
   dev.new()
-  plot(env_func(t), x$f.lamb(t), type='l', xlab="Environmental data", ylab="speciation rate", main="Fitted speciation rate")
+  plot(env_func(t), fit.env$f.lamb(t), type='l', xlab="Environmental data", ylab="speciation rate", main="Fitted speciation rate")
 
 
   if ("f.mu" %in% attributes(x))
   {
     # Attribute f.mu ==> not fixed extinction
     dev.new()
-    plot(-t, x$f.mu(t), type='l', xlab="time", ylab="extinction rate", main="Fitted extinction rate")
-    plot(env_func(t), x$f.mu(t), type='l', xlab="Environmental data", ylab="extinction rate", main="Fitted extinction rate")
-    r <- function(t) {x$f.lamb(t) - x$f.mu(t)}
+    plot(-t, fit.env$f.mu(t), type='l', xlab="time", ylab="extinction rate", main="Fitted extinction rate")
+    plot(env_func(t), fit.env$f.mu(t), type='l', xlab="Environmental data", ylab="extinction rate", main="Fitted extinction rate")
+    r <- function(t) {fit.env$f.lamb(t) - x$f.mu(t)}
     dev.new()
     plot(-t, r(t), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
   dev.new()
@@ -28,8 +28,8 @@ plot_fit_env <- function(fit.env, env_data, tot_time)
   else
   {
     dev.new()
-    plot(-t, x$f.lamb(t), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
+    plot(-t, fit.env$f.lamb(t), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
     dev.new()
-    plot(env_func(t), x$f.lamb(t), type='l', xlab="Environmental data", ylab="net diversification rate", main="Fitted net diversification rate")
+    plot(env_func(t), fit.env$f.lamb(t), type='l', xlab="Environmental data", ylab="net diversification rate", main="Fitted net diversification rate")
   }
 }
