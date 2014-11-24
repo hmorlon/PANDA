4904f7082317679375816147785e94c60037d7be
Helene Morlon <morlon@biologie.ens.fr>
2014-11-24 16:26:30 +0100

    minor edits to likelihood_sgd function

--- R/likelihood_sgd.R
+++ R/likelihood_sgd.R
@@ -1,8 +1,7 @@
 likelihood_sgd <- function(phylo, tot_time, b, d, nu, f)
 {
-	# The function that everybody should use : transforms the object of type "phylo" into lambert representation, then returns the total likelihood of the tree
-	lambert = Phylo2Lambert(phylo)
+	lambert <- Phylo2Lambert(phylo)
 	lambert[1] <- tot_time
-	likeli <- LikelihoodSGDFromLambert(lambert, b, d, nu, f)
-	return(likeli[3]+likeli[4])
+	likel <- LikelihoodSGDFromLambert(lambert, b, d, nu, f)
+	return(likel[3]+likel[4])
 }
