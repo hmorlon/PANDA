b8b0a4d101bd7f83d622c656ceb235f36f0372e8
Helene Morlon <morlon@biologie.ens.fr>
2014-11-24 12:25:58 +0100

    replace fit_sgd function and manual after Marc's modifications to account for negative parameters

--- R/fit_sgd.R
+++ R/fit_sgd.R
@@ -1,16 +1,16 @@
 fit_sgd <- function(phylo, tot_time, par_init, f=1)
 {
 	#par_init contains the initial values of parameters (b, d, nu), phylo is an object of class phylo
-	lambert <- Phylo2Lambert(phylo)
+	lambert = Phylo2Lambert(phylo)
 	lambert[1] <- tot_time
 
 	optim_LikelihoodSGD <- function(par)
 	{
-		likeli = LikelihoodSGDFromLambert(lambert, par[1], par[2], par[3], f)
+		likeli = LikelihoodSGDFromLambert(lambert, abs(par[1]), abs(par[2]), abs(par[3]), f)
 		L = likeli[3]+likeli[4]
 		return(-log(L))
 	}
 	
-	inference <- optim(par_init, optim_LikelihoodSGD)
-	return(inference)
+	inference <- optim(par_init, optim_LikelihoodSGD)$par
+	return( c(birth = abs(inference[1]), growth = abs(inference[1])-abs(inference[2]), mutation = abs(inference[3])) )
 }
