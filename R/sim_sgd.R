376b267a49d0fb9d753c87449d621684edfae7eb
Helene Morlon <morlon@biologie.ens.fr>
2014-11-24 16:27:41 +0100

    minor edits to sim_sgd function

--- R/sim_sgd.R
+++ R/sim_sgd.R
@@ -1,6 +1,6 @@
 sim_sgd <- function(tau, b, d, nu)
 {
-	lambert = Simulate_SGD_Phylogeny(tau, 0, 2, b, d, nu)
-	phylo = read.tree(text = paste(Lambert2Newick(lambert), ";", sep=""))
+	lambert <- Simulate_SGD_Phylogeny(tau, 0, 2, b, d, nu)
+	phylo <- read.tree(text = paste(Lambert2Newick(lambert), ";", sep=""))
 	return(phylo)
 }
