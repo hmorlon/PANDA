7fb501428e582060c6e8d836d054e416d22303e8
Helene Morlon <morlon@biologie.ens.fr>
2014-11-25 17:18:29 +0100

    modifs to sim_sgd function to account for phylogenies with only 1 species

--- R/sim_sgd.R
+++ R/sim_sgd.R
@@ -1,7 +1,14 @@
 sim_sgd <- function(tau, b, d, nu)
 {
 	lambert <- Simulate_SGD_Phylogeny(tau, 0, 2, b, d, nu)
-	phylo <- read.tree(text = paste(Lambert2Newick(lambert), ";", sep=""))
+	if(length(lambert) == 1)
+	{
+		newick <- paste("(", Lambert2Newick(lambert), ");", sep="")
+	}
+	else{
+		newick <- paste(Lambert2Newick(lambert), ";", sep="")
+	}
+	phylo <- read.tree(text = newick)
 	phylo$tip.label <- paste("s", 1:Ntip(phylo), sep = "")
 	return(phylo)
 }
