cb35b2736527e9078b74d57addf2b81dfd63a16c
Denis Barbier <barbier@imacs.polytechnique.fr>
2015-01-30 14:42:19 +0100

    Optimized version

--- R/ou_deSolve.R
+++ R/ou_deSolve.R
@@ -16,47 +16,35 @@ paste(rep(TWOLETTERS,each=26),LETTERS,sep="")->THREELETTERS
 
 
 ##define a few useful variables
-##nodeDist, the distance between each node and the root
-##nodeDiff, the distance between each node and the previous node (times for integration)
+##nodeDist, the distance between each inner node and the root
+##nodeDiff, the distance between each inner node and the previous node (times for integration)
 
-nodeDist<-vector()
+nodeDist<-vector(mode = "numeric", length = phylo$Nnode)
 root <- length(phylo$tip.label) + 1
 heights<-nodeHeights(phylo)
-for (i in root:(length(phylo$tip.label)+phylo$Nnode)){
-	if(i==root){
-	nodeDist[i-length(phylo$tip.label)]<-0
-	} else {
-	int<-heights[match(i,phylo$edge[,1]),1]
-	nodeDist[i-length(phylo$tip.label)]<-int
-	}
+for (i in 1:dim(phylo$edge)[1]){
+	nodeDist[[phylo$edge[i, 1] - length(phylo$tip.label)]] <- heights[i]
 }
 nodeDist<-c(nodeDist,max(heights))
 nodeDiff<-diff(nodeDist)
 ##label the branches for each segment of tree to be integrated and identify the node at which the branch terminates
 
-mat<-matrix(ncol=3)
+mat<-matrix(nrow=0, ncol=3)
+counter_three_letters <- 0
 for(i in 1:phylo$Nnode){
-	branches<-phylo$edge[phylo$edge[,1]==i+length(phylo$tip.label),]
-	for(j in 1:2){
-		if(branches[j,2]>length(phylo$tip.label)){
-			int<-matrix(ncol=3)
-			int[1]<-i+length(phylo$tip.label)
-			int[2]<-paste(".",THREELETTERS[1],sep="")
-			int[3]<-phylo$edge[which(phylo$edge[,1]==i+length(phylo$tip.label)),][j,2]
-			k<-1
-			while(!is.na(match(int[2],mat))){
-				int[2]<-paste(".",THREELETTERS[k],sep="")
-				k<-k+1
-				}
+	other<-phylo$edge[phylo$edge[,1]==i+length(phylo$tip.label), 2]
+	for(b in other){
+		int<-matrix(ncol=3)
+		int[1]<-i+length(phylo$tip.label)
+		if(b>length(phylo$tip.label)){
+			counter_three_letters <- counter_three_letters + 1
+			int[2]<-paste(".",THREELETTERS[counter_three_letters],sep="")
+			int[3]<-b
 			} else {
-			int<-matrix(ncol=3)
-			int[1]<-i+length(phylo$tip.label)
-			int[2]<-phylo$tip.label[branches[j,2]]
+			int[2]<-phylo$tip.label[[b]]
 			int[3]<-0 ##NOTE :I am considering tips to be "0" here and use this below
 			}
-		if(is.na(mat[1])){
-		mat<-int} else {
-		mat<-rbind(mat,int)}
+		mat<-rbind(mat,int)
 		}
 	}		
 
@@ -81,62 +69,66 @@ for(i in 2:length(nodeDiff)){			##THIS LOOP checks for an error
 
 #### NOW DEFINE ODEs for each interval and numerically integrate from root to tip, one interval at at a time ###
 output<-list() ##initialize storage of results from ODEs 
+env.results<-new.env(size = 1, parent = emptyenv())
 
 for(i in 1:phylo$Nnode){ ##for each interval between branches...
 
 ##var.list is the list of terms for which there is a variance term at each step in time
 ##cov.list is the list of covariance terms
 var.list<-unlist(nat[[i]])
-cov.list<-apply(t(combn(unlist(nat[[i]]),2)),1,paste,collapse="_")
+cov.list<-apply(t(combn(var.list,2)),1,paste,collapse="_")
 Cmat<-matrix(nrow=length(var.list),ncol=length(var.list)) ##"Cmat" is a lower triangle matrix with each of the variance and covariance terms, used to define equations below
 diag(Cmat)<-var.list
 Cmat[lower.tri(Cmat)]<-cov.list
 
 ##define ODEs for deSOLVE
- ou<-function(t, state, parameters) {
- with(as.list(c(state, parameters)),{
- # rate of change
-
 	##this loop defines the set of ODEs for the variance terms (stored in "var.list"), one for each "extant branch" of the phylo
 	##note: if you want to visualize the output of this, replace the line beginning with 'eval(' with:
 	##then call each term in var.list (i.e., in var.list) to see what the ODE for that term is
-	for(m in 1:length(var.list)){
-		sumint<-intersect(Cmat[,m],Cmat[lower.tri(Cmat)])
-		if(m>1){ #unless it is the first term in varlist, it is necessary to coerce the script to extract the lower triangle version of a covariance term (e.g., AB instead of BA)
-			sumint<-c(sumint, Cmat[m,1:(m-1)])
-			}
-		sumterm<-paste(sumint,collapse=",")		
-	 	eval(parse(text=paste("d",var.list[m],"<--(((((2*(length(var.list)-1))/length(var.list)*s))+(2*a))*",var.list[m],")+(((2*s)/length(var.list))*sum(",sumterm,"))+b",sep="")))
-	 	#assign(var.list[m],paste("-(((((2*(length(var.list)-1))/length(var.list)*s))+(2*a))*",var.list[m],")+(((2*s)/length(var.list))*sum(",sumterm,"))+b",sep=""))
-	 	}	
-	 
+	len <- length(var.list)
+	dim <- len * (len + 1) / 2
+	ind <- matrix(0, nrow = len, ncol=len, byrow=TRUE)
+	indI <- vector(mode = "integer", length = dim)
+	indJ <- vector(mode = "integer", length = dim)
+	for(k in 1:len) {
+	    ind[[k,k]] <- k
+	    indI[[k]] <- k
+	    indJ[[k]] <- k
+	}
+	counter <- len
+	for(l in 1:(len-1)) {
+	    for(k in (l+1):len) {
+	        counter <- counter + 1
+	        ind[[k,l]] <- counter
+	        ind[[l,k]] <- counter
+	        indI[[counter]] <- k
+	        indJ[[counter]] <- l
+	    }
+	}
+	coefAlpha <- (2*(len-1)/len * parameters['s'] + 2 * parameters['a'])
+	coefBeta <- parameters['s'] / len
+	rhs <- vector(mode = "numeric", length = dim)
+	for(m in 1:len) {
+	    rhs[m] <- parameters['b']
+	}
+	A <- diag(x = -coefAlpha, nrow = dim, ncol=dim)
+	coefBetaV <- rep(coefBeta, times = len)
+	for(ind_jk in 1:dim) {
+	    j <- indI[[ind_jk]]
+	    k <- indJ[[ind_jk]]
+	    coefBetaV[[k]] <- 0
+	    A[ind_jk, ind[j, ] ] <- A[ind_jk, ind[j, ] ] + coefBetaV
+	    coefBetaV[[k]] <- coefBeta
+	    coefBetaV[[j]] <- 0
+	    A[ind_jk, ind[k, ] ] <- A[ind_jk, ind[k, ] ] + coefBetaV
+	    coefBetaV[[j]] <- coefBeta
+	}
 
-	##this loop defines the set of ODEs for the covariance terms (stored in "cov.list"), only for the lower triangle elements (including diagonal) of the VCV matrix
-	##elements included for each covariance include ALL variance and covariance terms (other than the covariance being computed) involving the two component terms
-	##note: if you want to visualize the output of this, replace the line beginning with 'eval(' with:
-	##then call each term (i.e., in cov.list) to see what the ODE for that term is
-	for(m in 1:length(cov.list)){
-		sumint<-vector()
-		for(j in 1:2){
-		k<-which(Cmat==(unlist(strsplit(cov.list[m],"_"))[j]),arr.in=TRUE)[1]
-		sumint<-c(sumint,intersect(Cmat[,k],Cmat[lower.tri(Cmat,diag=TRUE)]))
-		if(k>1){
-			sumint<-c(sumint, Cmat[k,1:(k-1)])
-			}
-		}
-		sumint<-unique(sumint)[which(unique(sumint)!=cov.list[m])]
-		sumterm<-paste(sumint,collapse=",")	##NEED TO FIND OUT if the order of terms is important in these sum terms, if not no worries IF SO then this needs some work
-		eval(parse(text=paste("d",cov.list[m],"<-(-(((2*(length(var.list)-1))/length(var.list)*s)+2*a)*",cov.list[m],")+(((s)/length(var.list))*sum(",sumterm,"))",sep="")))
-		#assign(cov.list[m],paste("(-(((2*(length(var.list)-1))/length(var.list)*s)+2*a)*",cov.list[m],")+(((s)/length(var.list))*sum(",sumterm,"))",sep=""))
-		}
-				
+	 
  # return the rate of change
  sttrt=paste("d",c(var.list,cov.list),sep="",collapse=",")
  sttat=paste("list(c(",sttrt,"))",sep="")
- eval(parse(text=sttat))
- }) # end with(as.list ...
- }
- 
+
 ######	STARTING VALUES  ######
 #This next block of code provides the starting values for the ODEs by identifying the proper value from the previous integration
 if(i==1){ #if it is the first iteration, the starting values for all terms are 0
@@ -150,23 +142,20 @@ state.new<-paste("c(",start,")",sep="")
 state<-eval(parse(text=state.new))
 termlist<-c(var.list,cov.list)
 for(l in 1:length(termlist)){
-if(termlist[l] %in% colnames(output[[i-1]])){ #if the term to be numerically integrated was present in the previous generation(branch) of the tree
-	tmin1<-output[[i-1]][2,match(termlist[l],colnames(output[[i-1]]))] #simply use the last value from that numerical integration as the starting value
-	x<-match(termlist[l],row.names(as.data.frame(state)))  #and append that to the state vector
-	state[x]<-tmin1
+term<-termlist[l]
+if(exists(term, envir=env.results)){ #if the term to be numerically integrated was present in the previous generation(branch) of the tree
+       state[l]<-get(term, envir=env.results)
 	}  
 else { #if term is not present in previous generation/branch of the tree, lookup which branch it descends from
-	if(termlist[l] %in% var.list){ ##this loop looks up values for variance terms using the "mat" matrix that is produced earlier
-		prev.branch<-mat[mat[,3]==mat[mat[,2]==termlist[l],1],2]
-		tmin1<-output[[i-1]][2,match(prev.branch,colnames(output[[i-1]]))]
-		x<-match(termlist[l],row.names(as.data.frame(state))) 
-		state[x]<-tmin1
+	if(l <= length(var.list)){ ##this loop looks up values for variance terms using the "mat" matrix that is produced earlier
+		prev.branch<-mat[mat[,3]==mat[mat[,2]==term,1],2]
+		state[l]<-get(prev.branch, envir=env.results)
 	}
 	else{ ##for covariance terms that weren't present in previous generation/branch, it is necessary to decompose covariance term, look up terms appropriately, and re-assemble them in the correct order so they match the lower triangle format used in definition of terms
-		firstterm<-strsplit(termlist[l],"_")[[1]][1]
-		scondterm<-strsplit(termlist[l],"_")[[1]][2]
-		if(firstterm %in% colnames(output[[i-1]]) || scondterm %in% colnames(output[[i-1]])){
-			if(firstterm %in% colnames(output[[i-1]])){
+		firstterm<-strsplit(term,"_")[[1]][1]
+		scondterm<-strsplit(term,"_")[[1]][2]
+		if(exists(firstterm, envir=env.results) || exists(scondterm, envir=env.results)){
+			if(exists(firstterm, envir=env.results)){
 				prev.branch<-mat[mat[,3]==mat[mat[,2]==scondterm,1],2]
 				prev.branch2<-firstterm} 
 			else{  ##NOTE prev.branch2 just identifies which term was present previously
@@ -174,48 +163,50 @@ else { #if term is not present in previous generation/branch of the tree, lookup
 				prev.branch2<-scondterm
 				}
 		prev.term<-paste(prev.branch2,prev.branch,sep="_")
-		if(is.na(match(prev.term,colnames(output[[i-1]])))){
+		if(! exists(prev.term, envir=env.results)){
 			prev.term<-paste(prev.branch,prev.branch2,sep="_")
 			}
-		tmin1<-output[[i-1]][2,match(prev.term,colnames(output[[i-1]]))]
-		x<-match(termlist[l],row.names(as.data.frame(state))) 
-		state[x]<-tmin1
+		state[l]<-get(prev.term, envir=env.results)
 		} 				
 	else {
 		##neither first nor second term was present in the previous generation/branch, so the starting value is a variance term from t-1
 		prev.branch<-mat[mat[,3]==mat[mat[,2]==firstterm,1],2]
-		tmin1<-output[[i-1]][2,match(prev.branch,colnames(output[[i-1]]))]
-		x<-match(termlist[l],row.names(as.data.frame(state))) 
-		state[x]<-tmin1
+		state[l]<-get(prev.branch, envir=env.results)
 	}
 	}
 	}
 	}
 	}
 
+ou <- function(t, state, parameters) {
+  dX <- A %*% state + rhs
+  return (list(c(dX)))
+}
 
 ##NOW, run numerical integration for given time step and append to a list
-output[[i]]<-ode(y=state,times=c(0,nodeDiff[i]),func=ou,parms=parameters)
+output<-ode(y=state,times=c(0,nodeDiff[i]),func=ou,parms=NULL)
+colN <- colnames(output)
+env.results<-new.env(size = length(colN), parent = emptyenv())
+for (k in 2:length(colN)){
+  assign(colN[[k]], output[[2,k]], envir = env.results)
+}
 }
 
 
 #########	RETURN VCV MATRIX  	#########
 
 Vou<-mrca(phylo)
-diag(Vou)<-output[[length(nodeDiff)]][2,][2:(length(phylo$tip.label)+1)]
-for(j in (length(phylo$tip.label)+2):length(output[[length(nodeDiff)]][2,])){
+diag(Vou)<-output[2,][2:(length(phylo$tip.label)+1)]
+for(j in (length(phylo$tip.label)+2):length(output[2,])){
 
-	string<-strsplit(names(output[[length(nodeDiff)]][2,j]),"_")
+	string<-strsplit(names(output[2,j]),"_")
 	string<-unlist(string)
 	one<-match(string[1],colnames(Vou))
 	two<-match(string[2],colnames(Vou))
-	if(lower.tri(Vou)[one,two]==TRUE){
-		Vou[one,two]<-output[[length(nodeDiff)]][2,j]} else{
-		Vou[two,one]<-output[[length(nodeDiff)]][2,j]
-	}
+	Vou[one,two]<-output[2,j]
+	Vou[two,one]<-output[2,j]
 }
 
-Vou[upper.tri(Vou)]<-t(Vou)[upper.tri(t(Vou))]
 return(Vou)
 }
 
