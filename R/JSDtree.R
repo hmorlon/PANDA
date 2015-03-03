8cce61f358f0e85e7983915c3d45df39cc27beb2
elewitus <elewitus@users.noreply.github.com>
2015-03-03 15:22:41 +0100

    Update and rename R to R/JSDtree.R

---
+++ R/JSDtree.R
@@ -0,0 +1,82 @@
+#get Jensen-Shannon divergence	
+JSDtree <- function(trees,method=c("standard","normal")){
+	dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
+	KLD <- function(x,y) sum(x*log(x/y))
+	JSD <- function(x,y) sqrt(0.5*KLD(x,(x+y)/2)+0.5*KLD(y,(x+y)/2))
+		matrixColSize <- length(colnames(inMatrix))
+		matrixRowSize <- length(rownames(inMatrix))
+		colnames <- colnames(inMatrix)
+	resultsMatrix <- matrix(0, matrixColSize, matrixColSize) 
+	  	inMatrix = apply(inMatrix,1:2,
+  		function(x) ifelse (x==0,pseudocount,x))
+	for(i in 1:matrixColSize) {
+		for(j in 1:matrixColSize) { 
+			resultsMatrix[i,j]=JSD(as.vector(inMatrix[,i]),
+			as.vector(inMatrix[,j]))
+		}
+	}
+	colnames -> colnames(resultsMatrix) -> rownames(resultsMatrix)
+	as.dist(resultsMatrix)->resultsMatrix
+	attr(resultsMatrix, "method") <- "dist"
+	return(resultsMatrix) 
+ }
+#gaussian kernel convolution	 
+dens <- function(x, bw = bw.nrd0, kernel = kernelG, n = 4096,
+                from = min(x) - 3*sd, to = max(x) + 3*sd, adjust = 1,
+                ...) {
+  if(has.na <- any(is.na(x))) {
+    na.omit(x)->x
+    if(length(x) == 0)
+        stop('too infinite.')
+  }
+  	kernelG<-function(x, mean=0, sd=1) 
+		dnorm(x, mean = mean, sd = sd)
+  x<-log(x)		
+  sd <- (if(is.numeric(bw)) bw[1] else bw(x)) * adjust
+  X <- seq(from, to, len = n)
+  M <- outer(X, x, kernel, sd = sd, ...)
+  structure(list(x = X, y = rowMeans(M), bw = sd,
+                 call = match.call(), n = length(x),
+                 data.name = deparse(substitute(x)),
+                 has.na = has.na), class =  "density")
+}
+#take square-root of Jensen-Shannon divergence
+JSDist <- function(x,y) sqrt(dist.JSD(x,y))
+	
+#compute eigenvalues for phylogenies and convolve with Gaussian kernel	
+	treeNodes <- lapply(trees,dist.nodes)	 
+	treeMats <- lapply(treeNodes,data.matrix)
+	treeGraphs <- lapply(treeMats,graph.adjacency,weighted=T)
+	treeLaplacian <- lapply(treeGraphs,graph.laplacian,
+					normalized=F)			
+	treeEigen <- lapply(treeLaplacian,eigen,
+				symmetric=TRUE,only.values=TRUE)	
+	m<-c()
+	d<-c()
+		if(method=="standard"){
+		for(i in 1:length(treeEigen)){
+			m[[i]]<-subset(treeEigen[[i]]$values,
+				treeEigen[[i]]$values>=1)
+			d[[i]]<-dens(m[[i]])	
+		}
+	}	
+		if(method=="normal"){
+		for(i in 1:length(treeEigen)){
+			m[[i]]<-subset(treeEigen[[i]]$values,
+				treeEigen[[i]]$values>=1)
+			d[[i]]<-dens(m[[i]]/length(m[[i]]))	
+		}
+	}	
+	Ds<-c()
+		for(i in 1:length(d)){
+			Ds<-cbind(Ds,d[[i]]$x)
+		}
+#write divergence metrics to file			
+	write.table(Ds,"JSDMatrix.txt")
+	DS<-read.table("JSDMatrix.txt",header=T)
+	JSD<-as.matrix(JSDist(DS))
+	write.table(JSD,"JSDMatrix.txt")
+
+#print heatmap	
+heatmap(JSD)	
+}
