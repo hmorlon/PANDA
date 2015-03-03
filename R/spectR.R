897cdd213cec3e49905bbfd742a23e2fd8db9570
elewitus <elewitus@users.noreply.github.com>
2015-03-03 15:21:36 +0100

    Update and rename R to R/spectR.R

---
+++ R/spectR.R
@@ -0,0 +1,92 @@
+require(geiger)
+require(igraph)
+
+
+#plot spectral density
+spectR <- function(phylo,method=c("standard","normal")){
+		
+		if(!is.phylo(phylo))
+		{
+			stop('object is not phylo.')
+		}
+
+#gaussian kernel convolution		
+dens <- function(x, bw = bw.nrd0, kernel = kernelG, n = 4096,
+                from = min(x) - 3*sd, to = max(x) + 3*sd, adjust = 1,
+                ...) {
+  if(has.na <- any(is.na(x))) {
+    na.omit(x)->x
+    if(length(x) == 0)
+        stop('too infinite.')
+  }
+  	kernelG<-function(x, mean=0, sd=1) 
+		dnorm(x, mean = mean, sd = sd)
+  x<-log(x)		
+  sd <- (if(is.numeric(bw)) bw[1] else bw(x)) * adjust
+  X <- seq(from, to, len = n)
+  M <- outer(X, x, kernel, sd = sd, ...)
+  structure(list(x = X, y = rowMeans(M), bw = sd,
+                 call = match.call(), n = length(x),
+                 data.name = deparse(substitute(x)),
+                 has.na = has.na), class =  "density")
+}
+
+#integration
+integr <- function(x, f)
+{
+       if (!is.numeric(x))
+       {
+              stop('"x" is not numeric.')
+       }
+       if (!is.numeric(f))
+       {
+              stop('"f" is not numeric.')
+       }
+       if (length(x) != length(f))
+       {
+              stop('integration variable and integrand are wrong for each other.')
+       }
+
+       length(x)->n
+       integral=0.5*sum((x[2:n]-x[1:(n-1)])*(f[2:n]+f[1:(n-1)]))
+       return(integral)
+}
+
+		e=eigen(
+			graph.laplacian(
+				graph.adjacency(
+					data.matrix(
+						dist.nodes(phylo))
+					,weighted=T)
+				,normalized=F)
+			,symmetric=T,only.values=F) #set to T to skip eigenvectors
+		m=subset(e$values,e$values>=1)
+	
+		#get eigengap
+		abs(diff(m))->gaps
+			as.matrix(gaps)->gapMat
+				c(1:length(gapMat))->extraCol
+			cbind(extraCol,gapMat)->gapMatCol
+		subset(gapMatCol,gapMatCol[,2]==max(gapMatCol[,2]))->eigenGap
+		
+		#get spectral density
+		if(method=="standard"){
+		dens(m)->d}
+		if(method=="normal"){
+			dens(m/length(m))->d
+		}
+			integr(d$x,d$y)->dint
+				(d$y/dint)->dsc
+
+		#plot eigengap,spectral density
+		par(mfrow=c(1,2))
+			plot(d$x,dsc,type="l",ann=F)
+				mtext(expression(f*(x)/integral(f(y)*dy)),2,2)
+					mtext("ln eigenvalue",1,2)
+			plot(sort(log(m),decreasing=T),ann=F)
+				mtext("rank",1,2)
+					mtext("ln eigenvalue",2,3)
+					
+return(eigenGap[,1])
+}
+
