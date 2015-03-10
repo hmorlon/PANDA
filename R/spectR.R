7d6158494b0ee76a782e24f27014d2a4e0585d0d
elewitus <elewitus@users.noreply.github.com>
2015-03-10 19:21:18 +0100

    Update spectR.R

--- R/spectR.R
+++ R/spectR.R
@@ -21,7 +21,11 @@ dens <- function(x, bw = bw.nrd0, kernel = kernelG, n = 4096,
   }
   	kernelG<-function(x, mean=0, sd=1) 
 		dnorm(x, mean = mean, sd = sd)
-  x<-log(x)		
+if(method=="standard"){		
+  x<-log(x)	}
+if(method=="normal"){
+  x<-x
+ } 	
   sd <- (if(is.numeric(bw)) bw[1] else bw(x)) * adjust
   X <- seq(from, to, len = n)
   M <- outer(X, x, kernel, sd = sd, ...)
@@ -51,7 +55,7 @@ integr <- function(x, f)
        integral=0.5*sum((x[2:n]-x[1:(n-1)])*(f[2:n]+f[1:(n-1)]))
        return(integral)
 }
-
+	if(method=="standard"){
 		e=eigen(
 			graph.laplacian(
 				graph.adjacency(
@@ -59,10 +63,9 @@ integr <- function(x, f)
 						dist.nodes(phylo))
 					,weighted=T)
 				,normalized=F)
-			,symmetric=T,only.values=F) #set to T to skip eigenvectors
+			,symmetric=T,only.values=F)
 		m=subset(e$values,e$values>=1)
-	
-		#get eigengap
+	#get eigengap
 		abs(diff(m))->gaps
 			as.matrix(gaps)->gapMat
 				c(1:length(gapMat))->modalities
@@ -70,8 +73,7 @@ integr <- function(x, f)
 		subset(gapMatCol,gapMatCol[,2]==max(gapMatCol[,2]))->eigenGap
 		
 		#get spectral density
-		if(method=="standard"){
-			dens(m)->d
+		dens(m)->d
 			integr(d$x,d$y)->dint
 				(d$y/dint)->dsc
 
@@ -82,25 +84,37 @@ integr <- function(x, f)
 					mtext("ln eigenvalue",1,2)
 			plot(sort(log(m),decreasing=T),ann=F)
 				mtext("rank",1,2)
-					mtext("ln eigenvalue",2,3)
-		}
-		if(method=="normal"){
-			dens(m/length(m))->d
+					mtext("ln eigenvalue",2,3)	
+		return(eigenGap[,1])			
+	}
+	
+	if(method=="normal"){
+		e=eigen(
+			graph.laplacian(
+				graph.adjacency(
+					data.matrix(
+						dist.nodes(phylo))
+					,weighted=T)
+				,normalized=T)
+			,symmetric=T,only.values=F)
+		m=e$values
+
+		#get eigengap
+		abs(diff(m))->gaps
+			as.matrix(gaps)->gapMat
+				c(1:length(gapMat))->modalities
+			cbind(modalities,gapMat)->gapMatCol
+		subset(gapMatCol,gapMatCol[,2]==max(gapMatCol[,2]))->eigenGap
+		
+		#get spectral density
+		dens(m)->d
 			integr(d$x,d$y)->dint
 				(d$y/dint)->dsc
 
-		#plot eigengap,spectral density
-		par(mfrow=c(1,2))
+		#plot spectral density
 			plot(d$x,dsc,type="l",ann=F)
 				mtext(expression(f*(x)/integral(f(y)*dy)),2,2)
 					mtext("ln eigenvalue",1,2)
-			plot(sort(log(m/length(m)),decreasing=T),ann=F)
-				mtext("rank",1,2)
-					mtext("ln eigenvalue",2,3)
-		}
-					
-return(eigenGap[,1])
+		}					
 }
-
-
 
