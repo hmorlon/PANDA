58dd810057a36153e510930679bb81fd09c06f6f
Eric Lewitus <lewitus@biologie.ens.fr>
2015-05-04 14:27:02 +0200

    update usage

--- R/JSDtree.R
+++ R/JSDtree.R
@@ -1,7 +1,5 @@
-require(geiger)
-require(igraph)
 #get Jensen-Shannon divergence	
-JSDtree <- function(trees,method=c("standard","normal")){
+JSDtree <- function(phylo,method=c("standard","normal")){
 	dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
 	KLD <- function(x,y) sum(x*log(x/y))
 	JSD <- function(x,y) sqrt(0.5*KLD(x,(x+y)/2)+0.5*KLD(y,(x+y)/2))
