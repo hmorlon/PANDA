5353278e73f091964dfd69012a2ab5847e48e8f9
elewitus <lewitus@biologie.ens.fr>
2015-05-25 07:57:39 +0200

    suppress "did not converge" warnings, fixed poisson option

--- R/BICompare.R
+++ R/BICompare.R
@@ -1,5 +1,6 @@
 #get BIC values for tree matrix and random matrix for 1:t modalities
 BICompare <- function(phylo,t,method=c("gaussian","poisson")){
+	options(warn=-1)
 	#get lLk, BIC
 	kmeansBIC <- function(fit){
 		m <- ncol(fit$centers)
@@ -9,26 +10,27 @@ BICompare <- function(phylo,t,method=c("gaussian","poisson")){
 	return(data.frame(BIC = D + log(n)*m*k))
 	}
 		phyloM <- as.matrix(dist.nodes(phylo))	
-		DP <- c()
+		rDP <- c()
 		if(method=="gaussian"){
-			rDP = matrix(rnorm(length(phyloM),median(phyloM),							3*sd(phyloM)))#gaussian
-				}
+			rDP = matrix(rnorm(length(phyloM),median(phyloM),							3*sd(phyloM)))
+				
 		matrix()->r
 		matrix()->p				
 		for(i in c(1:t)){	
 			kmeansBIC(kmeans(rDP,t,algorithm="Hartigan-Wong"))->r[i]
 			kmeansBIC(kmeans(phyloM,t,algorithm="Hartigan-Wong"))->p[i]
 			}
-
+		}
 		if(method=="poisson"){
-			rDP = matrix(rpois(length(phyloM),median(phyloM)))#poisson
-				}
+			rDP = matrix(rpois(length(phyloM),median(phyloM)))
+				
 		for(i in c(1:t)){	
 			kmeansBIC(kmeans(rDP,t,algorithm="Lloyd"))->r[i]
 			kmeansBIC(kmeans(phyloM,t,algorithm="Lloyd"))->p[i]
 			}
-				
+	}			
 rp<-cbind(p,r)
 colnames(rp)<-c("tree BIC","control BIC")
 return(rp)
+
 }
