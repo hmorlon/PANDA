a51f7cf5b5e6040ca084ff1ddc983aeefb7b0718
Eric Lewitus <lewitus@biologie.ens.fr>
2015-05-26 16:05:21 +0200

    remove "poisson" option

--- R/BICompare.R
+++ R/BICompare.R
@@ -1,5 +1,5 @@
 #get BIC values for tree matrix and random matrix for 1:t modalities
-BICompare <- function(phylo,t,method=c("gaussian","poisson")){
+BICompare <- function(phylo,t){
 	options(warn=-1)
 	#get lLk, BIC
 	kmeansBIC <- function(fit){
@@ -13,18 +13,9 @@ BICompare <- function(phylo,t,method=c("gaussian","poisson")){
 		rDP <- c()
 		c()->r
 		c()->p	
-		if(method=="gaussian"){
 			rDP = matrix(rnorm(length(phyloM),median(phyloM),							3*sd(phyloM)))	
-
 			kmeansBIC(kmeans(rDP,t,algorithm="Hartigan-Wong"))->r
 			kmeansBIC(kmeans(phyloM,t,algorithm="Hartigan-Wong"))->p
-	}
-		if(method=="poisson"){
-			rDP = matrix(rpois(length(phyloM),median(phyloM)))
-
-			kmeansBIC(kmeans(rDP,t,algorithm="Lloyd"))->r
-			kmeansBIC(kmeans(phyloM,t,algorithm="Lloyd"))->p
-	}			
 rp<-cbind(p,r)
 colnames(rp)<-c("tree BIC","control BIC")
 return(rp)
