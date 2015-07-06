d3b4744954367fd1bdeb9007369a7ba5dde157e1
Jonathan Drury <jonathan.p.drury@gmail.com>
2015-07-06 14:10:02 +0200

    fix typo

--- R/likelihood_MC_geog.R
+++ R/likelihood_MC_geog.R
@@ -3,7 +3,7 @@ likelihood_MC_geog<-function(phylo,data,par=c(sig2,S),geography.object) #par[1]=
   	if(length(par)!=2){stop("par must contain two values, one for sig2 and another for S")}
   	sig2<-abs(par[1]) 
   	sterm<-ifelse(sign(par[2])==-1,par[2],-par[2])
-	V<-try(.VCV.rescale.geog(phylo,sig2,0,sterm,geography.matrix))
+	V<-try(.VCV.rescale.geog(phylo,sig2,0,sterm,geography.object))
 	if(class(V)=="try-error"){return(Inf)}
 	if(any(is.na(V))){
 		return(Inf)
