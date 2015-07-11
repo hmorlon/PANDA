dec2bf621065b449ccb0f9ae41bb2aa45dc75960
elewitus <lewitus@biologie.ens.fr>
2015-07-11 08:51:41 +0200

    include plotting of hierarchical tree showing bootstrap support

--- R/plot_JSDtree.R
+++ R/plot_JSDtree.R
@@ -1,9 +1,16 @@
-plot_JSDtree <- function(JSDtree)
+plot_JSDtree <- function(JSDtree,alpha)
 {
   if (!inherits(JSDtree, "JSDtree"))
       stop("object \"JSDtree\" is not of class \"JSDtree\"")
 
+#plot heatmap
 heatmap(JSDtree$JSD,symm=T)
-  
+
+#plot hierarchical clustering with bootstrap support
+quartz()
+	clustersHierarchy <- pvclust(JSDtree$JSD)
+	plot(clustersHierarchy,cex=0.5)
+		pvrect(clustersHierarchy,alpha=alpha)
+
   }
 
