9efed58bfaf502337d8a09a2f8785325a82ed98e
Eric Lewitus <lewitus@biologie.ens.fr>
2015-07-15 15:39:41 +0200

    include k-medoid output

--- R/JSDtree_cluster.R
+++ R/JSDtree_cluster.R
@@ -3,6 +3,10 @@ plot_JSDtree <- function(JSDtree,alpha)
   if (!inherits(JSDtree, "JSDtree"))
       stop("object \"JSDtree\" is not of class \"JSDtree\"")
 
+#cluster JSD matrix on medoids
+clustersMedoid <- pamk(JSDtree$JSD)
+	clustersMedoidSupport <- pam(JSDtree$JSD,clustersMedoid$nc)
+
 #plot heatmap
 heatmap(JSDtree$JSD,symm=T)
 
@@ -11,6 +15,10 @@ quartz()
 	clustersHierarchy <- pvclust(JSDtree$JSD)
 	plot(clustersHierarchy,cex=0.5)
 		pvrect(clustersHierarchy,alpha=alpha)
+
+#print clustersMedoid
+res	<- list(clusters=clustersMedoid$nc, cluster_assignments=clustersMedoid[[1]][[3]],cluster_support=clustersMedoidSupport[7])
+return(res)
 
   }
 
