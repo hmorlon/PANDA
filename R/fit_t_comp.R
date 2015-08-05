0a949b971ac90764b8bedbf86082df3b779493a2
Helene Morlon <morlon@biologie.ens.fr>
2015-08-05 18:06:09 +0200

    small edits

--- R/fit_t_comp.R
+++ R/fit_t_comp.R
@@ -8,17 +8,17 @@ if(is.null(par)){par<-c(var(data)/max(nodeHeights(phylo)),0)}
 if(is.null(geography.object)){
 	if(model=="MC"){
 		opt<-optim(par,likelihood_t_MC,phylo=phylo,data=data,method=method)
-		results<-list(lnL = -opt$value, sig2 = abs(opt$par[1]), S = opt$par[2], aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, convergence = opt$convergence)
+		results<-list(LH = -opt$value, aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, sig2 = abs(opt$par[1]), S = opt$par[2], convergence = opt$convergence)
 		return(results)
 		}
 	if(model=="DDexp"){
 		opt<-optim(par,likelihood_t_DD,phylo=phylo,data=data,model="DDexp",method=method)
-		results<-list(lnL = -opt$value, sig2 = abs(opt$par[1]), r = opt$par[2], aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, convergence = opt$convergence)
+		results<-list(LH = -opt$value, aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, sig2 = abs(opt$par[1]), r = opt$par[2], convergence = opt$convergence)
 		return(results)
 		}
 	if(model=="DDlin"){
 		opt<-optim(par,likelihood_t_DD,phylo=phylo,data=data,model="DDlin",method=method)
-		results<-list(lnL = -opt$value, sig2 = abs(opt$par[1]), b = opt$par[2], aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, convergence = opt$convergence)
+		results<-list(LH = -opt$value, aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, sig2 = abs(opt$par[1]), b = opt$par[2], convergence = opt$convergence)
 		return(results)
 		}
 }
@@ -28,17 +28,17 @@ if(!is.null(geography.object)){
 	if(length(geography.object)!=phylo$Nnode){stop("geography object cannot have more or fewer components than internode intervals in phylo")}
 	if(model=="MC"){
 		opt<-optim(par,likelihood_t_MC_geog,phylo=phylo,geography.object=geography.object,data=data,method=method)
-		results<-list(lnL = -opt$value, sig2 = abs(opt$par[1]), S = opt$par[2], aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, convergence = opt$convergence)
+		results<-list(LH = -opt$value, aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, sig2 = abs(opt$par[1]), S = opt$par[2], convergence = opt$convergence)
 		return(results)
 		}
 	if(model=="DDexp"){
 		opt<-optim(par,likelihood_t_DD_geog,phylo=phylo,geography.object=geography.object,data=data,model="DDexp",method=method)
-		results<-list(lnL = -opt$value, sig2 = exp(opt$par[1]), r = opt$par[2], aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, convergence = opt$convergence)
+		results<-list(LH = -opt$value, aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, sig2 = exp(opt$par[1]), r = opt$par[2], convergence = opt$convergence)
 		return(results)
 		}
 	if(model=="DDlin"){
 		opt<-optim(par,likelihood_t_DD_geog,phylo=phylo,geography.object=geography.object,data=data,model="DDlin",method=method)
-		results<-list(lnL = -opt$value, sig2 = exp(opt$par[1]), b = opt$par[2], aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, convergence = opt$convergence)
+		results<-list(LH = -opt$value, aic = (2*3 - 2*(-opt$value)), aicc = (2*3 - 2*(-opt$value))+((2*3*(3+1))/(length(phylo$tip.label)-3-1)), free.paramaters = 3, sig2 = exp(opt$par[1]), b = opt$par[2], convergence = opt$convergence)
 		return(results)
 		}
 
