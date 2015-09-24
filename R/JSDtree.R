hmorlon/PANDA

commit 344d0004d2e2f739848bfefd92d5018b610738ce

replace trees by phylo

R/JSDtree.R
@@ -72,7 +72,7 @@ JSDist <- function(x,y) sqrt(dist.JSD(x,y))
 	}	
 	
 		if(meth=="normal1"){
-	treeNodes <- lapply(trees,dist.nodes)	 
+	treeNodes <- lapply(phylo),dist.nodes)	 
 	treeMats <- lapply(treeNodes,data.matrix)
 	treeGraphs <- lapply(treeMats,graph.adjacency,weighted=T)
 	treeLaplacian <- lapply(treeGraphs,graph.laplacian,
@@ -98,7 +98,7 @@ JSDist <- function(x,y) sqrt(dist.JSD(x,y))
 	}
 	
 	if(meth=="normal2"){
-		treeNodes <- lapply(trees,dist.nodes)	 
+		treeNodes <- lapply(phylo,dist.nodes)	 
 		treeMats <- lapply(treeNodes,data.matrix)
 		treeGraphs <- lapply(treeMats,graph.adjacency,weighted=T)
 		treeLaplacian <- lapply(treeGraphs,graph.laplacian,

commit a31a40fb5fe1c7493fbfff032e5620c24608bfc5

Fix typo introduced in 344d000

R/JSDtree.R
@@ -72,7 +72,7 @@ JSDist <- function(x,y) sqrt(dist.JSD(x,y))
 	}	
 	
 		if(meth=="normal1"){
-	treeNodes <- lapply(phylo),dist.nodes)	 
+	treeNodes <- lapply(phylo,dist.nodes)	 
 	treeMats <- lapply(treeNodes,data.matrix)
 	treeGraphs <- lapply(treeMats,graph.adjacency,weighted=T)
 	treeLaplacian <- lapply(treeGraphs,graph.laplacian,