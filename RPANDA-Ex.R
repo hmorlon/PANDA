e74cb8c9c6cddaf8d42f9a5f769ae249bca7606a
Jonathan Drury <jonathan.p.drury@gmail.com>
2015-11-20 17:19:43 +0100

    typo fix

--- RPANDA-Ex.R
+++ RPANDA-Ex.R
@@ -177,7 +177,7 @@ flush(stderr()); flush(stdout())
 
 trees<-TESS::tess.sim.age(n=20,age=10,0.15,0.05,MRCA=TRUE)
 res<-JSDtree(trees)
-#JSDtree_cluster(res,alpha=0.9)
+#JSDtree_cluster(res,alpha=0.9,draw=T)
 
 
 
