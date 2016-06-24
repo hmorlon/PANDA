de8c56e96c300b54f289749b1f04a18dfb09dbcc
Florian Hartig <florianhartig@users.noreply.github.com>
2016-06-24 20:18:49 +0200

    mcmc implementation initial

--- R/fit_bd.R
+++ R/fit_bd.R
@@ -2,7 +2,7 @@ fit_bd <-
  function (phylo, tot_time, f.lamb, f.mu, lamb_par, mu_par, f=1,
            meth = "Nelder-Mead", cst.lamb=FALSE, cst.mu=FALSE,
            expo.lamb=FALSE, expo.mu=FALSE, fix.mu=FALSE,
-           dt=0, cond="crown")
+           dt=0, cond="crown", mcmc = F, mcmcSettings = NULL, prior = NULL)
 {
   if (!inherits(phylo, "phylo"))
       stop("object \"phylo\" is not of class \"phylo\"")
@@ -48,6 +48,18 @@ fit_bd <-
     f.mu.par <- function(t){f.mu(t, mu_par)}
     res <- list(model = "birth.death", LH = -temp$value, aicc=2*temp$value+2*p+(2*p*(p+1))/(nobs-p-1),lamb_par=lamb.par, f.lamb=Vectorize(f.lamb.par))
   }
+  
+  if (mcmc==TRUE){
+    if(fix.mu==FALSE) mcmcSettings$startValue = c(lamb.par, mu.par)
+    LL <- function(par) -optimLH(par)
+    names = paste("lambda", 1:length(lamb.par))
+    if (fix.mu==FALSE) names = c(names, paste("mu", 1:length(mu.par)))
+    if(is.null(prior)) prior = BayesianTools::createUniformPrior(lower = rep(0,p), upper = rep(20,p))
+    BayesianSetup = BayesianTools::createBayesianSetup(likelihood = LL, prior = prior, names = names)
+    MCMCres<-BayesianTools::runMCMC(bayesianSetup = BayesianSetup, sampler = "DEzs", settings = mcmcSettings)
+    res$mcmc = MCMCres
+  }
+  
   class(res) <- "fit.bd"
   return(res)
 }
