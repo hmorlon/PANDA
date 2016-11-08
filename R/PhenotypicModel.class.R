c37fcb87d7cb6258c3e4cf7996d0b5e8c253ab5f
mmanceau <marc.manceau@gmail.com>
2016-11-08 09:41:31 +0100

    adding the same warning in the model comparison function.

--- R/PhenotypicModel.class.R
+++ R/PhenotypicModel.class.R
@@ -402,6 +402,7 @@ setMethod(
     definition=function(object, data){
         cat("*** Model selection with tip trait data ***\n")
         cat("For each model in \"object\", fits the model and returns its AIC value in a recap table...\n")
+        cat("**WARNING** : This function relies on the standard R optimizer \"optim\".\nIt may not always converge well.\nPlease double check the convergence by trying\n distinct parameter sets for the initialisation.")
 
         aic <- c()
         names <- c()
