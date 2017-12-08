c636ac6bd7b7a1537a4c827dda28214dea49902c
elewitus <elewitus@users.noreply.github.com>
2017-12-08 19:12:43 +0100

    Update spectR.R

--- R/spectR.R
+++ R/spectR.R
@@ -1,32 +1,7 @@
 spectR<-function (phylo, method = c("standard")) 
-{
-    kurtosis.sub <- function(x, na.rm = FALSE, method = c("moment"), 
-        ...) {
-        method = match.arg(method)
-        stopifnot(NCOL(x) == 1)
-        if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
-            warning("argument is not numeric or logical: returning NA")
-            return(as.numeric(NA))
-        }
-        if (na.rm) 
-            x = x[!is.na(x)]
-        n = length(x)
-        if (is.integer(x)) 
-            x = as.numeric(x)
-        if (method == "moment") {
-            kurtosis = sum((x - mean(x))^4/as.numeric(var(x))^2)/length(x)
-        }
-        if (method == "excess") {
-            kurtosis = sum((x - mean(x))^4/var(x)^2)/length(x) - 
-                3
-        }
-        if (method == "fisher") {
-            kurtosis = ((n + 1) * (n - 1) * ((sum(x^4)/n)/(sum(x^2)/n)^2 - 
-                (3 * (n - 1))/(n + 1)))/((n - 2) * (n - 3))
-        }
-        kurtosis
-    }
-    skewness <- function(x, na.rm = FALSE) {
+			{
+#define skewness function				
+skewness <- function(x, na.rm = FALSE) {
         if (is.matrix(x)) 
             apply(x, 2, skewness, na.rm = na.rm)
         else if (is.vector(x)) {
@@ -39,36 +14,22 @@ spectR<-function (phylo, method = c("standard"))
             sapply(x, skewness, na.rm = na.rm)
         else skewness(as.vector(x), na.rm = na.rm)
     }
-        integr <- function(x, f) {
-            if (!is.numeric(x)) {
-                stop("The variable of integration \"x\" is not numeric.")
-            }
-            if (!is.numeric(f)) {
-                stop("The integrand \"f\" is not numeric.")
-            }
-            if (length(x) != length(f)) {
-                stop("The lengths of the variable of integration and the integrand do not match.")
-            }
-            n = length(x)
-            integral = 0.5 * sum((x[2:n] - x[1:(n - 1)]) * (f[2:n] + 
-                f[1:(n - 1)]))
-            return(integral)
-        }
-        dens <- function(x, bw = bw.nrd0, kernel = kernelG, n = 4096,
+
+#define density function
+##gaussian kernel
+sigma = 0.1
+gKernel <- function(x) 1/(sigma*sqrt(2*pi)) * exp(-(x^2)/2*sigma^2)
+kernelG <- function(x, mean=0, sd=1) dnorm(x, mean = mean, sd = sd)
+
+##kernel density estimate
+dens <- function(x, bw = bw.nrd0, kernel = kernelG, n = 4096,
                 from = min(x) - 3*sd, to = max(x) + 3*sd, adjust = 1,
                 ...) {
   if(has.na <- any(is.na(x))) {
-    na.omit(x)->x
+    x <- na.omit(x)
     if(length(x) == 0)
-        stop('too infinite.')
+        stop("no finite or non-missing data!")
   }
-  	kernelG<-function(x, mean=0, sd=1) 
-		dnorm(x, mean = mean, sd = sd)
-if(method=="standard"){		
-  x<-log(x)	}
-if(method=="normal"){
-  x<-x
- } 	
   sd <- (if(is.numeric(bw)) bw[1] else bw(x)) * adjust
   X <- seq(from, to, len = n)
   M <- outer(X, x, kernel, sd = sd, ...)
@@ -78,47 +39,71 @@ if(method=="normal"){
                  has.na = has.na), class =  "density")
 }
 
-    
-    if (method == "standard") {
-        e = eigen(graph.laplacian(graph.adjacency(data.matrix(dist.nodes(phylo)), 
-            weighted = T), normalized = F), symmetric = T, only.values = F)
-        m = subset(e$values, e$values >= 1)
-        d = dens(log(m))
-        dsc = d$y/integr(d$x, d$y)
-        gaps <- abs(diff(m))
-        gapMat <- as.matrix(gaps)
-        modalities <- c(1:length(gapMat))
-        gapMatCol <- cbind(modalities, gapMat)
-        eigenGap <- subset(gapMatCol, gapMatCol[, 2] == max(gapMatCol[, 
-            2]))
-        principal_eigenvalue <- max(m)
-        kurtosis <- kurtosis.sub(x)
-        skewness <- skewness(x)
-        peak_height <- max(dsc)
-        res <- list(eigenvalues = e$values, principal_eigenvalue = principal_eigenvalue, 
-            asymmetry = skewness, peakedness1 = kurtosis, peakedness2 = peak_height, 
-            eigengap = eigenGap[, 1])
-    }
-    if (method == "normal") {
-        e = eigen(graph.laplacian(graph.adjacency(data.matrix(dist.nodes(phylo)), 
-            weighted = T), normalized = T), symmetric = T, only.values = F)
-        m = subset(e$values, e$values >= 0)
-        d = dens(m)
-        dsc = d$y/integr(d$x, d$y)
-        gaps <- abs(diff(m))
-        gapMat <- as.matrix(gaps)
-        modalities <- c(1:length(gapMat))
-        gapMatCol <- cbind(modalities, gapMat)
-        eigenGap <- subset(gapMatCol, gapMatCol[, 2] == max(gapMatCol[, 
-            2]))
-        principal_eigenvalue <- max(m)
-        kurtosis <- kurtosis.sub(x)
-        skewness <- skewness(x)
-        peak_height <- max(dsc)
-        res <- list(eigenvalues = m, principal_eigenvalue = principal_eigenvalue, 
-            asymmetry = skewness, peakedness1 = kurtosis, peakedness2 = peak_height, 
-            eigengap = eigenGap[, 1])
-    }
-    class(res) <- "spectR"
-    return(res)
+#define integral function
+integr <- function(x, f)
+{
+       
+       # var is numeric
+       if (!is.numeric(x))
+       {
+              stop('The variable of integration "x" is not numeric.')
+       }
+
+       # integrand is numeric
+       if (!is.numeric(f))
+       {
+              stop('The integrand "f" is not numeric.')
+       }
+
+       # length(var)=length(int)
+       if (length(x) != length(f))
+       {
+              stop('The lengths of the variable of integration and the integrand do not match.')
+       }
+
+      # get lengths of var and integrand
+       n = length(x)
+
+       # trapezoidal integration
+       integral = 0.5*sum((x[2:n] - x[1:(n-1)]) * (f[2:n] + f[1:(n-1)]))
+
+       # print definite integral
+       return(integral)
 }
+ if (method == "standard") {
+e = eigen(graph.laplacian(graph.adjacency(data.matrix(dist.nodes(phylo)), 
+            weighted = T), normalized = F), symmetric = T, only.values = T)
+	x = subset(e$values, e$values >= 1)
+	d = dens(log(x))
+	dsc = d$y/(integr(d$x,d$y))
+	principal_eigenvalue <- max(x)
+	skewness <- skewness(x)
+    peak_height <- max(dsc)
+	gaps<-abs(diff(x))
+        gapMat <- as.matrix(gaps)
+        modalities <- c(1:length(gapMat))
+        gapMatCol <- cbind(modalities, gapMat)
+        eigenGap <- subset(gapMatCol, gapMatCol[, 2] == max(gapMatCol[,2]))
+	res<-list(eigenvalues=x,principal_eigenvalue=principal_eigenvalue, 
+            asymmetry=skewness, peakedness=peak_height,eigengap= eigenGap[,1])   
+	}
+ if (method == "normal") {
+e = eigen(graph.laplacian(graph.adjacency(data.matrix(dist.nodes(phylo)), 
+            weighted = T), normalized = T), symmetric = T, only.values = T)
+	x = subset(e$values, e$values >= 0)
+	d = dens(log(x))
+	dsc = d$y/(integr(d$x,d$y))
+	principal_eigenvalue <- max(x)
+	skewness <- skewness(x)
+    peak_height <- max(dsc)
+	gaps <- abs(diff(x))
+        gapMat <- as.matrix(gaps)
+        modalities <- c(1:length(gapMat))
+        gapMatCol <- cbind(modalities, gapMat)
+        eigenGap <- subset(gapMatCol, gapMatCol[, 2] == max(gapMatCol[,2]))
+	res<-list(eigenvalues=x,principal_eigenvalue=principal_eigenvalue, 
+            asymmetry=skewness, peakedness=peak_height,eigengap= eigenGap[,1])    
+		}
+	class(res) <- "spectR"
+    return(res)
+}    	
