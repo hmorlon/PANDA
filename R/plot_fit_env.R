61dbf303b1e85a1b400006067c817c0206f93488
hmorlon <morlon.helene@gmail.com>
2018-08-10 10:58:13 +0200

    Update plot_fit_env.R
    Remove "type=l" plotting option to avoid spurious lines in plots when same environmental value reached at different time points

--- R/plot_fit_env.R
+++ R/plot_fit_env.R
@@ -10,7 +10,7 @@ plot_fit_env <- function(fit.env, env_data, tot_time)
   spline_result <- sm.spline(env_data[,1],env_data[,2], df=df)
   env_func <- function(t){predict(spline_result,t)}
   dev.new()
-  plot(env_func(t), fit.env$f.lamb(t), type='l', xlab="Environmental data", ylab="speciation rate", main="Fitted speciation rate")
+  plot(env_func(t), fit.env$f.lamb(t), xlab="Environmental data", ylab="speciation rate", main="Fitted speciation rate")
 
 
   if ("f.mu" %in% attributes(fit.env)$names)
@@ -19,18 +19,18 @@ plot_fit_env <- function(fit.env, env_data, tot_time)
     dev.new()
     plot(-t, fit.env$f.mu(t), type='l', xlab="time", ylab="extinction rate", main="Fitted extinction rate")
     dev.new()
-    plot(env_func(t), fit.env$f.mu(t), type='l', xlab="Environmental data", ylab="extinction rate", main="Fitted extinction rate")
+    plot(env_func(t), fit.env$f.mu(t), xlab="Environmental data", ylab="extinction rate", main="Fitted extinction rate")
     r <- function(t) {fit.env$f.lamb(t) - fit.env$f.mu(t)}
     dev.new()
     plot(-t, r(t), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
   dev.new()
-    plot(env_func(t), r(t), type='l', xlab="Environmental data", ylab="net diversification rate", main="Fitted net diversification rate")
+    plot(env_func(t), r(t), xlab="Environmental data", ylab="net diversification rate", main="Fitted net diversification rate")
   }
   else
   {
     dev.new()
     plot(-t, fit.env$f.lamb(t), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
     dev.new()
-    plot(env_func(t), fit.env$f.lamb(t), type='l', xlab="Environmental data", ylab="net diversification rate", main="Fitted net diversification rate")
+    plot(env_func(t), fit.env$f.lamb(t), xlab="Environmental data", ylab="net diversification rate", main="Fitted net diversification rate")
   }
 }
