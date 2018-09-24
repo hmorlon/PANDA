5fc4e692046e779384d46679076d386e6c4e9033
Jonathan Drury <jonathan.p.drury@gmail.com>
2018-09-24 15:16:16 +0200

    speed up

--- R/trimSimmap.R
+++ R/trimSimmap.R
@@ -1,13 +1,17 @@
-#This is a script that trims a stochastic map to preserve only those branches containing a given category (according to the algorithm in Drury et al. MS)
+#This is a script that trims a stochastic map to preserve only those branches containing a given category
 #The inputs are:
-#map (simmap object that is a stochastic map of discrete traits used to identify group of competitors) 
+#map (simmap object that is a stochastic map of discrete traits used to identify guild of competitors) 
 #trim.class (discrete category identifying species in 'data' (e.g., herbivores))
 
 .trimSimmap<-function(map,trim.class){
 		trc=trim.class
 		smap<-map
+		
+		repeat{
+		
 		tot.len<-length(smap$tip.label)
 		node = tot.len + 1
+		tbdropped<-c()
 		
 		while(node <= (tot.len+smap$Nnode)){
 			descL<-smap$edge[which(smap$edge[,1]==node),2][1]
@@ -16,26 +20,21 @@
 			if(!any(sapply(smap$maps[Lclade],function(x)any(names(x)%in%trc)))){ #left descendants DO NOT contain target trait
 		
 				if(length(Lclade)==1){ 	#if only descendant is tip, drop entire branch
-					smap<-drop.tip.simmap(smap,smap$tip.label[descL])
-					tot.len<-length(smap$tip.label)
-					node = tot.len + 1
-					
-				} else{		#if descendant is clade, drop all but one branch (randomly)
-					Ltips<-smap$edge[Lclade[which(smap$edge[Lclade,2]<=tot.len)],2]	
-					smap<-drop.tip.simmap(smap,smap$tip.label[sample(Ltips,length(Ltips)-1)])
-					tot.len<-length(smap$tip.label)
-					node = tot.len + 1
+					tbdropped<-c(tbdropped,smap$tip.label[descL])					
 				}
 		
-			} else {
+			}
 			
 			node = node + 1
 			
-			}
 			}	
 		
+		smap<-drop.tip.simmap(smap,tbdropped)
+		smapL<-smap
+					
+		tot.len<-length(smap$tip.label)
 		node = tot.len + 1
-		
+		tbdropped<-c()
 		
 		while(node <= (tot.len+smap$Nnode)){
 			descR<-smap$edge[which(smap$edge[,1]==node),2][2]
@@ -44,23 +43,25 @@
 			if(!any(sapply(smap$maps[Rclade],function(x)any(names(x)%in%trc)))){ #right descendants DO NOT contain target trait
 		
 				if(length(Rclade)==1){ 	#if only descendant is tip, drop entire branch
-					smap<-drop.tip.simmap(smap,smap$tip.label[descR])
-					tot.len<-length(smap$tip.label)
-					node = tot.len + 1
+					tbdropped<-c(tbdropped,smap$tip.label[descR])
 					
-				} else{		#if descendant is clade, drop all but one branch (randomly)
-					Rtips<-smap$edge[Rclade[which(smap$edge[Rclade,2]<=tot.len)],2]	
-					smap<-drop.tip.simmap(smap,smap$tip.label[sample(Rtips,length(Rtips)-1)])
-					tot.len<-length(smap$tip.label)
-					node = tot.len + 1
-				}
-		
-			} else {
+				} 
+				
+			}
 			
-			node = node + 1
+			node = node +1
 			
 			}
-			}	
+			
+		smap<-drop.tip.simmap(smap,tbdropped)
+		
+		if(length(smapL$tip.label)==length(smap$tip.label)){
+		break
+		}
+		
+		
+		}
+					
 	return(smap)
 	}	
 
