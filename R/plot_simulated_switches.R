53b15026b509df204e9f7985936a75514ccfd54a
Benoît Perez-Lamarque <benoit.perez.lamarque@gmail.com>
2018-12-12 20:31:28 +0100

    Update plot_simulated_switches.R

--- R/plot_simulated_switches.R
+++ R/plot_simulated_switches.R
@@ -1,6 +1,7 @@
 plot_simulated_switches <-
 function(n,host_tree,name,index,switches,...){ 
-  
+  if(!exists("path")) {path <- getwd()}
+  setwd(path)
   pdf(paste("/figures/host_tree_switches_",name,"_",index,".pdf",sep=""))
   tree <- ladderize(host_tree)
   for(i in 1:n){tree$tip.label[i] <- paste("    ",tree$tip.label[i],sep="")}
