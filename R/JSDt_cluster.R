bc9be64e9ed5e2925c8b10040ed58f50d2f90e54
elewitus <elewitus@users.noreply.github.com>
2019-05-31 11:27:17 -0400

    Update and rename JSDcluster_t.R to JSDt_cluster.R

--- R/JSDt_cluster.R
+++ R/JSDt_cluster.R
@@ -1,7 +1,7 @@
 
 
 #get Jensen-Shannon divergence	
-JSDcluster_t <- function(phylo,mat,plot=F){
+JSDt_cluster <- function(phylo,mat,plot=F){
 	dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
 	KLD <- function(x,y) sum(x*log(x/y))
 	JSD <- function(x,y) sqrt(0.5*KLD(x,(x+y)/2)+0.5*KLD(y,(x+y)/2))
