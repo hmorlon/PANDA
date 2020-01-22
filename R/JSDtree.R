f3cb64d9497d90e7a1d2245b9dc40f0f617c0086
elewitus <elewitus@users.noreply.github.com>
2020-01-22 08:14:04 -0500

    revert to proper function name

--- R/JSDtree.R
+++ R/JSDtree.R
@@ -1,4 +1,4 @@
-JSDtreeX <- function(phylo,meth=c("standard")){
+JSDtree <- function(phylo,meth=c("standard")){
 	dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
 	KLD <- function(x,y) sum(x*log(x/y))
 	JSD <- function(x,y) sqrt(0.5*KLD(x,(x+y)/2)+0.5*KLD(y,(x+y)/2))
