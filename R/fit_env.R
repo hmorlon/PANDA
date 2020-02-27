5c10a294a2091d47c073152cfabb17b53d5d42fe
hmorlon <morlon.helene@gmail.com>
2020-02-27 14:50:27 +0100

    Update fit_env.R

--- R/fit_env.R
+++ R/fit_env.R
@@ -2,7 +2,7 @@ fit_env <- function (phylo, env_data, tot_time, f.lamb, f.mu, lamb_par, mu_par, 
            meth = "Nelder-Mead", cst.lamb=FALSE, cst.mu=FALSE, expo.lamb=FALSE,
            expo.mu=FALSE, fix.mu=FALSE, dt=0, cond="crown")
 {
-  if (tot_time > max(env_data[1,]))
+  if (tot_time > max(env_data[,1]))
   {
     stop("The environmental data does not cover the time span of the phylogeny: either enter data that covers the full time span or run analyses on younger clades")
   }
