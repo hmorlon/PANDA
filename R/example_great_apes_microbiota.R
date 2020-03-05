6a43814642e7804ee9664c12defea74c0d011589
Benoît Perez-Lamarque <44650654+BPerezLamarque@users.noreply.github.com>
2020-03-05 15:27:08 +0100

    Revert "add HOME"

--- R/example_great_apes_microbiota.R
+++
@@ -1,8 +0,0 @@
-example_great_apes_microbiota <-
-function(name,path=getwd()){ 
-  data(great_apes_microbiota)
-  write.tree(host_tree,file = paste(path,"/host_tree_",name,".tre",sep=""))
-  write.dna(alignment_OTU0001,paste(path,"/alignment_",name,"_OTU0001.fas",sep=""), format = "fasta",nbcol = -1,colsep="",colw=ncol(alignment_OTU0001))
-  write.dna(alignment_OTU0002,paste(path,"/alignment_",name,"_OTU0002.fas",sep=""), format = "fasta",nbcol = -1,colsep="",colw=ncol(alignment_OTU0002))
-  write.dna(alignment_OTU0003,paste(path,"/alignment_",name,"_OTU0003.fas",sep=""), format = "fasta",nbcol = -1,colsep="",colw=ncol(alignment_OTU0003))
-}
--- R/rescale_tree.R
+++
@@ -1,4 +0,0 @@
-rescale_tree <-
-function(tree){
-  tree$edge.length <- tree$edge.length/sum(tree$edge.length)
-  return(tree)}
