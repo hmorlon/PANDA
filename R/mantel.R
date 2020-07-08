f27e21ec76ab1ab3e6ce2fee7cb27db9c739d90d
Benoît Perez-Lamarque <benoit.perez.lamarque@gmail.com>
2020-07-08 11:27:41 +0200

    Update mantel.R

--- R/mantel.R
+++ R/mantel.R
@@ -51,14 +51,16 @@ function (formula = formula(data), data = sys.parent(), nperm = 1000, correlatio
       cresults <- .C("permute", as.double(xmat), as.double(ymat), 
                      as.integer(n), as.integer(length(xmat)), as.integer(nperm), 
                      zstats = as.double(zstats), as.double(as.vector(tmat)), 
-                     as.integer(rarray))
+                     as.integer(rarray),
+                     PACKAGE = "RPANDA")
     }
       
     if (correlation=="Kendall"){
       cresults <- .C("permuteKendall", as.double(xmat), as.double(ymat),
                      as.integer(n), as.integer(length(xmat)), as.integer(nperm), 
                      zstats = as.double(zstats), as.double(as.vector(tmat)), 
-                     as.integer(rarray))
+                     as.integer(rarray),
+                     PACKAGE = "RPANDA")
     }
       
     zstats <- cresults$zstats
