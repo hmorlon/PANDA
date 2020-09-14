bd529355e783d053791d6718edbd39940443dfe2
Benoît Perez-Lamarque <benoit.perez.lamarque@gmail.com>
2020-09-14 13:48:46 +0200

    minor

--- R/model_selection_HOME.R
+++ R/model_selection_HOME.R
@@ -12,13 +12,14 @@ function(index,name,nb_tree=10000,lambda=c(1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,2
     if (N_variant>0){
       output <- selection_vertical_transmission(name,index)
       run <- 1
+      nb_run <- 0
       if (file.exists(paste("results/model_selection_independent_",name,"_",index,".txt",sep=""))){
         if (overwrite==TRUE){ file.remove(paste("results/model_selection_independent_",name,"_",index,".txt",sep=""))
-        }else{ if (nrow(read.table(paste("results/model_selection_independent_",name,"_",index,".txt",sep=""),header=T))==nb_random) {run <- 0 }}}
+        }else{ if (nrow(read.table(paste("results/model_selection_independent_",name,"_",index,".txt",sep=""),header=T))==nb_random) {
+          run <- 0 }else{nb_run <- nrow(read.table(paste("results/model_selection_independent_",name,"_",index,".txt",sep=""),header=T))}}}
       if (run == 1){
-        for (replicate in 1:nb_random){
+        for (replicate in (nb_run+1):nb_random){
           print(noquote(paste("Replicate: ",replicate,sep="")))
           output <- independent_evolution(replicate,name,index,seed,nb_tree,lambda,nb_cores)} 
-        table <- read.table(paste("results/model_selection_independent_",name,"_",index,".txt",sep=""),header=T)
-      }else{print(noquote("overwrite==FALSE: model selection is not performed again"))}
+          }else{print(noquote("overwrite==FALSE: model selection is not performed again"))}
     }}}
