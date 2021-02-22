345072095c1703a65d3a267135180890e9f69bcc
hmorlon <morlon.helene@gmail.com>
2021-02-22 11:20:12 +0100

    use sapply to avoid issues with constant rates

--- R/plot_fit_bd.R
+++ R/plot_fit_bd.R
@@ -4,20 +4,20 @@ plot_fit_bd <- function(fit.bd,tot_time)
       stop("object \"fit.bd\" is not of class \"fit.bd\"")
   t <- seq(0,tot_time, length.out=100)
   dev.new()
-  plot(-t, fit.bd$f.lamb(t), type='l', xlab="time", ylab="speciation rate", main="Fitted speciation rate")
+  plot(-t, sapply(t,fit.bd$f.lamb), type='l', xlab="time", ylab="speciation rate", main="Fitted speciation rate")
 
   if ("f.mu" %in% attributes(fit.bd)$names)
   {
     # Attribute f.mu ==> not fixed extinction
     dev.new()
-    plot(-t, fit.bd$f.mu(t), type='l', xlab="time", ylab="extinction rate", main="Fitted extinction rate")
+    plot(-t, sapply(t,fit.bd$f.mu), type='l', xlab="time", ylab="extinction rate", main="Fitted extinction rate")
     r <- function(t) {fit.bd$f.lamb(t) - fit.bd$f.mu(t)}
     dev.new()
-    plot(-t, r(t), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
+    plot(-t, sapply(t,r), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
   }
   else
   {
     dev.new()
-    plot(-t, fit.bd$f.lamb(t), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
+    plot(-t, sapply(t,fit.bd$f.lamb), type='l', xlab="time", ylab="net diversification rate", main="Fitted net diversification rate")
   }
 }
