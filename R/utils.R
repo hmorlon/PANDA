f288b8e384370a0412f97c0e713374c4be50a472
hmorlon <morlon.helene@gmail.com>
2021-11-05 19:20:52 +0100

    Add rmvnorm

--- R/utils.R
+++ R/utils.R
@@ -73,3 +73,43 @@ Posdef <- function (p, ev = rexp(p, 1/100)) {
   Z <- t(O) %*% diag(ev) %*% O
   return(Z)
 }
+
+# --- Function to simulate multivariate normal distribution
+# From the mvtnorm package 
+rmvnorm_util<-function (n, mean = rep(0, nrow(sigma)), sigma = diag(length(mean)), 
+    method = c("eigen", "svd", "chol"), pre0.9_9994 = FALSE, 
+    checkSymmetry = TRUE) 
+{
+    if (checkSymmetry && !isSymmetric(sigma, tol = sqrt(.Machine$double.eps), 
+        check.attributes = FALSE)) {
+        stop("sigma must be a symmetric matrix")
+    }
+    if (length(mean) != nrow(sigma)) 
+        stop("mean and sigma have non-conforming size")
+    method <- match.arg(method)
+    R <- if (method == "eigen") {
+        ev <- eigen(sigma, symmetric = TRUE)
+        if (!all(ev$values >= -sqrt(.Machine$double.eps) * abs(ev$values[1]))) {
+            warning("sigma is numerically not positive semidefinite")
+        }
+        t(ev$vectors %*% (t(ev$vectors) * sqrt(pmax(ev$values, 
+            0))))
+    }
+    else if (method == "svd") {
+        s. <- svd(sigma)
+        if (!all(s.$d >= -sqrt(.Machine$double.eps) * abs(s.$d[1]))) {
+            warning("sigma is numerically not positive semidefinite")
+        }
+        t(s.$v %*% (t(s.$u) * sqrt(pmax(s.$d, 0))))
+    }
+    else if (method == "chol") {
+        R <- chol(sigma, pivot = TRUE)
+        R[, order(attr(R, "pivot"))]
+    }
+    retval <- matrix(rnorm(n * ncol(sigma)), nrow = n, byrow = !pre0.9_9994) %*% 
+        R
+    retval <- sweep(retval, 2, mean, "+")
+    colnames(retval) <- names(mean)
+    retval
+}
+
