8aa98e9eb25a958c2a1b55e6bc512f926ff36890
JClavel <julien.clavel@hotmail.fr>
2022-02-14 15:37:16 +0100

    Update fit_t_standard.R
    Option for fossil trees

--- R/fit_t_standard.R
+++ R/fit_t_standard.R
@@ -41,8 +41,9 @@ fit_t_standard <- function(phylo, data, model=c("BM","OU","EB","BMtrend"), error
   }
   
   # for OU1 et random root
-  if(model=="OU" && two.regime==FALSE && fixedRoot==FALSE){
-      precalc<-mv.Precalc(phylo, nb.traits=1, param=list(model="OU1", root=FALSE))
+  if(model=="OU" && two.regime==FALSE){
+      if(!is.ultrametric(phylo)) root = TRUE
+      precalc<-mv.Precalc(phylo, nb.traits=1, param=list(model="OU1", root=root))
   }
   
   if(two.regime){
