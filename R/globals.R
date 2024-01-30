4d2da30f1d118ec0ec8887bc887c2d24f190c960
Nathan Mazet <nathanmazet@gmail.com>
2024-01-30 09:37:08 +0100

    fix: add file for global variables

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("seq_time", "comb.shift", "phylo", "ALL_clade_names",
+                         "ALL_branch_times_clades", "backbone.option", "models",
+                         "n.max", "rate.max", "sampling.fractions", "functionWrapper"))
