1e80b41b582b50e58f43a6f2ed8f61bbf23888be
Benoît Perez-Lamarque <benoit.perez.lamarque@gmail.com>
2024-06-05 15:58:42 +0200

    Update fit_bd_in_past.R

--- R/fit_bd_in_past.R
+++ R/fit_bd_in_past.R
@@ -53,36 +53,36 @@
   ####### other dependencies ########
   
   if (dt==0){
-      # Compute using R integration functions
-      r <- function(t){f.lamb(t)-f.mu(t)}
-      r.int <- function(x,y){.Integrate(Vectorize(r),x,y,stop.on.error=FALSE)}
-      r.int.0 <- function(y){exp(r.int(0,y))*f.lamb(y)}
-      r.int.int <- function(x,y){.Integrate(Vectorize(r.int.0),x,y,stop.on.error=FALSE)}
-      rst <- r.int(s,t)
-      rist <- r.int.int(s,t)
-      ri0s <- r.int.int(0,s)
-      res <- exp(rst)*(abs(1+rist/(1/f+ri0s)))^(-2)
-      return(res)
-      
-    }else{
-      
-      Nintervals <- 1 + as.integer(t/dt)
-      X <- seq(0, t, length.out = Nintervals + 1)
-      r <- function(t){f.lamb(t)-f.mu(t)}
-      r.int <- cumsum(sapply(X, r)) * t / Nintervals
-      r.int.0 <- function(y){exp(r.int[1 + as.integer( y * Nintervals / t)]) * f.lamb(y)}
-      r.int.int.tab <- cumsum(sapply(X, r.int.0)) * t / Nintervals
-      r.int.int <- function(x,y){
-        indy <- 1 + as.integer( y * Nintervals / t)
-        indx <- 1 + as.integer( x * Nintervals / t)
-        value <- r.int.int.tab[indy] - r.int.int.tab[indx]
-        return(value)
-      }
-      rst <- r.int[1 + Nintervals] - r.int[1 + as.integer( s * Nintervals / t)]
-      rist <- r.int.int(s,t)
-      ri0s <- r.int.int(0,s)
-      res <- exp(rst)*(abs(1+rist/(1/f+ri0s)))^(-2)
-      return(res)
+    # Compute using R integration functions
+    r <- function(t){f.lamb(t)-f.mu(t)}
+    r.int <- function(x,y){.Integrate(Vectorize(r),x,y,stop.on.error=FALSE)}
+    r.int.0 <- function(y){exp(r.int(0,y))*f.lamb(y)}
+    r.int.int <- function(x,y){.Integrate(Vectorize(r.int.0),x,y,stop.on.error=FALSE)}
+    rst <- r.int(s,t)
+    rist <- r.int.int(s,t)
+    ri0s <- r.int.int(0,s)
+    res <- exp(rst)*(abs(1+rist/(1/f+ri0s)))^(-2)
+    return(res)
+    
+  }else{
+    
+    Nintervals <- 1 + as.integer(t/dt)
+    X <- seq(0, t, length.out = Nintervals + 1)
+    r <- function(t){f.lamb(t)-f.mu(t)}
+    r.int <- cumsum(sapply(X, r)) * t / Nintervals
+    r.int.0 <- function(y){exp(r.int[1 + as.integer( y * Nintervals / t)]) * f.lamb(y)}
+    r.int.int.tab <- cumsum(sapply(X, r.int.0)) * t / Nintervals
+    r.int.int <- function(x,y){
+      indy <- 1 + as.integer( y * Nintervals / t)
+      indx <- 1 + as.integer( x * Nintervals / t)
+      value <- r.int.int.tab[indy] - r.int.int.tab[indx]
+      return(value)
+    }
+    rst <- r.int[1 + Nintervals] - r.int[1 + as.integer( s * Nintervals / t)]
+    rist <- r.int.int(s,t)
+    ri0s <- r.int.int(0,s)
+    res <- exp(rst)*(abs(1+rist/(1/f+ri0s)))^(-2)
+    return(res)
   }
 }
 
@@ -229,65 +229,65 @@ likelihood_bd_in_past <- function(phylo,tot_time,time_stop,f.lamb,f.mu,desc, tot
 
 
 fit_bd_in_past <- function (phylo, tot_time, time_stop, f.lamb, f.mu, lamb_par, mu_par, desc, tot_desc,
-            meth = "Nelder-Mead", cst.lamb=FALSE, cst.mu=FALSE,
-            expo.lamb=FALSE, expo.mu=FALSE, fix.mu=FALSE,
-            dt=0, cond="crown") {
-    if (!inherits(phylo, "phylo"))
-      stop("object \"phylo\" is not of class \"phylo\"")
-
+                            meth = "Nelder-Mead", cst.lamb=FALSE, cst.mu=FALSE,
+                            expo.lamb=FALSE, expo.mu=FALSE, fix.mu=FALSE,
+                            dt=0, cond="crown") {
+  if (!inherits(phylo, "phylo"))
+    stop("object \"phylo\" is not of class \"phylo\"")
   
-    nobs <- Ntip(phylo)
+  
+  nobs <- Ntip(phylo)
+  
+  phylo$edge.length[phylo$edge[,2] %in% 1:nobs] <- phylo$edge.length[phylo$edge[,2] %in% 1:nobs] + time_stop
+  
+  if (fix.mu==FALSE){
     
-    phylo$edge.length[phylo$edge[,2] %in% 1:nobs] <- phylo$edge.length[phylo$edge[,2] %in% 1:nobs] + time_stop
-    
-    if (fix.mu==FALSE){
+    init <- c(lamb_par,mu_par)
+    p <- length(init)
+    optimLH <- function(init){
+      lamb_par <- init[1:length(lamb_par)]
+      mu_par <- init[(1+length(lamb_par)):length(init)]
+      f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
+      f.mu.par <- function(t){abs(f.mu(t,mu_par))}
       
-      init <- c(lamb_par,mu_par)
-      p <- length(init)
-      optimLH <- function(init){
-        lamb_par <- init[1:length(lamb_par)]
-        mu_par <- init[(1+length(lamb_par)):length(init)]
-        f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
-        f.mu.par <- function(t){abs(f.mu(t,mu_par))}
-        
-        LH <- likelihood_bd_in_past(phylo,tot_time,time_stop, f.lamb.par,f.mu.par,desc, tot_desc,cst.lamb=cst.lamb,cst.mu=cst.mu,expo.lamb=expo.lamb,expo.mu=expo.mu,dt=dt,cond=cond)
-
-        return(-LH)
-      }
-      temp <- suppressWarnings(optim(init, optimLH, method = meth))
-
-      lamb.par <- temp$par[1:length(lamb_par)]
-      mu.par <- temp$par[(1+length(lamb_par)):length(init)]
-      f.lamb.par <- function(t){abs(f.lamb(t, lamb.par))}
-      f.mu.par <- function(t){abs(f.mu(t, mu.par))}
-      res <- list(model = "birth death", LH = -temp$value, aicc=2*temp$value+2*p+(2*p*(p+1))/(nobs-p-1) , lamb_par=lamb.par, mu_par=mu.par, f.lamb=Vectorize(f.lamb.par), f.mu=Vectorize(f.mu.par))
-   
-       }else{
-         
-      init <- c(lamb_par)
-      p <- length(init)
-      optimLH <- function(init){
-        lamb_par <- init[1:length(lamb_par)]
-        f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
-        f.mu.par <- function(t){abs(f.mu(t,mu_par))}
-        LH <- likelihood_bd_in_past(phylo,tot_time,time_stop, f.lamb.par,f.mu.par,desc, tot_desc,cst.lamb=cst.lamb,cst.mu=TRUE,expo.lamb=expo.lamb,expo.mu=expo.mu,dt=dt,cond=cond)
-        return(-LH)
-      }
-      temp <- suppressWarnings(optim(init, optimLH, method = meth))
-      lamb.par <- temp$par[1:length(lamb_par)]
-      f.lamb.par <- function(t){abs(f.lamb(t, lamb.par))}
-      f.mu.par <- function(t){abs(f.mu(t, mu_par))}
-      res <- list(model = "pure birth", LH = -temp$value, aicc=2*temp$value+2*p+(2*p*(p+1))/(nobs-p-1),lamb_par=lamb.par, f.lamb=Vectorize(f.lamb.par))
+      LH <- likelihood_bd_in_past(phylo,tot_time,time_stop, f.lamb.par,f.mu.par,desc, tot_desc,cst.lamb=cst.lamb,cst.mu=cst.mu,expo.lamb=expo.lamb,expo.mu=expo.mu,dt=dt,cond=cond)
+      
+      return(-LH)
     }
-    class(res) <- "fit.bd"
-    return(res)
+    temp <- suppressWarnings(optim(init, optimLH, method = meth))
+    
+    lamb.par <- temp$par[1:length(lamb_par)]
+    mu.par <- temp$par[(1+length(lamb_par)):length(init)]
+    f.lamb.par <- function(t){abs(f.lamb(t, lamb.par))}
+    f.mu.par <- function(t){abs(f.mu(t, mu.par))}
+    res <- list(model = "birth death", LH = -temp$value, aicc=2*temp$value+2*p+(2*p*(p+1))/(nobs-p-1) , lamb_par=lamb.par, mu_par=mu.par, f.lamb=Vectorize(f.lamb.par), f.mu=Vectorize(f.mu.par))
+    
+  }else{
+    
+    init <- c(lamb_par)
+    p <- length(init)
+    optimLH <- function(init){
+      lamb_par <- init[1:length(lamb_par)]
+      f.lamb.par <- function(t){abs(f.lamb(t,lamb_par))}
+      f.mu.par <- function(t){abs(f.mu(t,mu_par))}
+      LH <- likelihood_bd_in_past(phylo,tot_time,time_stop, f.lamb.par,f.mu.par,desc, tot_desc,cst.lamb=cst.lamb,cst.mu=TRUE,expo.lamb=expo.lamb,expo.mu=expo.mu,dt=dt,cond=cond)
+      return(-LH)
+    }
+    temp <- suppressWarnings(optim(init, optimLH, method = meth))
+    lamb.par <- temp$par[1:length(lamb_par)]
+    f.lamb.par <- function(t){abs(f.lamb(t, lamb.par))}
+    f.mu.par <- function(t){abs(f.mu(t, mu_par))}
+    res <- list(model = "pure birth", LH = -temp$value, aicc=2*temp$value+2*p+(2*p*(p+1))/(nobs-p-1),lamb_par=lamb.par, f.lamb=Vectorize(f.lamb.par))
   }
+  class(res) <- "fit.bd"
+  return(res)
+}
 
 
 
 fit_env_in_past <- function (phylo, env_data, tot_time, time_stop, f.lamb,f.mu,lamb_par, mu_par, desc, tot_desc, df=NULL,
-                     meth = "Nelder-Mead", cst.lamb=FALSE, cst.mu=FALSE, expo.lamb=FALSE,
-                     expo.mu=FALSE, fix.mu=FALSE, dt=0, cond="crown"){
+                             meth = "Nelder-Mead", cst.lamb=FALSE, cst.mu=FALSE, expo.lamb=FALSE,
+                             expo.mu=FALSE, fix.mu=FALSE, dt=0, cond="crown"){
   if (tot_time > max(env_data[,1])){
     stop("The environmental data does not cover the time span of the phylogeny: either enter data that covers the full time span or run analyses on younger clades")
   }
@@ -321,7 +321,7 @@ fit_env_in_past <- function (phylo, env_data, tot_time, time_stop, f.lamb,f.mu,l
   f.lamb.env <- function(t,y){f.lamb(t, env_func_tab(t), y)}
   f.mu.env <- function(t,y){f.mu(t, env_func_tab(t), y)}
   res <- fit_bd_in_past(phylo, tot_time=tot_time, time_stop=time_stop, f.lamb=f.lamb.env, f.mu=f.mu.env, desc=desc, tot_desc=tot_desc, lamb_par=lamb_par, mu_par=mu_par,
-                         meth=meth, cst.lamb=cst.lamb, cst.mu=cst.mu, expo.lamb=expo.lamb, expo.mu=expo.mu, fix.mu=fix.mu, dt=dt, cond=cond)
+                        meth=meth, cst.lamb=cst.lamb, cst.mu=cst.mu, expo.lamb=expo.lamb, expo.mu=expo.mu, fix.mu=fix.mu, dt=dt, cond=cond)
   res$model <- "environmental birth death"
   res$f.lamb <- function(t){ abs(f.lamb(t, env_func_tab(t), res$lamb_par))}
   if (fix.mu==FALSE){
@@ -330,7 +330,3 @@ fit_env_in_past <- function (phylo, env_data, tot_time, time_stop, f.lamb,f.mu,l
   class(res) <- "fit.env"
   return(res)
 }
-
-
-
-
